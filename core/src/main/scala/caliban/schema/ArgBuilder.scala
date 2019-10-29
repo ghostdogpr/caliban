a5f4c31bd4c963005fe5e640fbf82028cfb8b27f
Shohei Shimomura <vividbit@gmail.com>
2019-10-29 20:58:01 +0900

    Add ArgBuilder#mapM (#61)
    * Add ArgBuilder#mapM
    
    * Improve ScalaDoc ArgBuilder#mapM

--- core/src/main/scala/caliban/schema/ArgBuilder.scala
+++ core/src/main/scala/caliban/schema/ArgBuilder.scala
@@ -24,6 +24,12 @@ trait ArgBuilder[T] { self =>
    * @param f a function from `T` to `A`.
    */
   def map[A](f: T => A): ArgBuilder[A] = (input: Value) => self.build(input).map(f)
+
+  /**
+   * Builds a new `ArgBuilder` of A from an existing `ArgBuilder` of `T` and a function from `T` to `IO[ExecutionError, A]`.
+   * @param f a function from `T` to IO[ExecutionError, A]
+   */
+  def mapM[A](f: T => IO[ExecutionError, A]): ArgBuilder[A] = (input: Value) => self.build(input).flatMap(f)
 }
 
 object ArgBuilder {
