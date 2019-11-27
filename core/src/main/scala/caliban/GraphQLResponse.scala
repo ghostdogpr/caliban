5d8ae9e407f053babe3bceeefa84769fec2d755d
Roman Timushev <rtimush@gmail.com>
2019-11-27 18:14:05 +0100

    Move circe codecs to the core module

--- core/src/main/scala/caliban/GraphQLResponse.scala
+++ core/src/main/scala/caliban/GraphQLResponse.scala
@@ -1,6 +1,28 @@
 package caliban
 
+import caliban.interop.circe._
+
+import scala.language.higherKinds
+
 /**
  * Represents the result of a GraphQL query, containing a data object and a list of errors.
  */
 case class GraphQLResponse[+E](data: ResponseValue, errors: List[E])
+
+object GraphQLResponse {
+  implicit def circeEncoder[F[_]: IsCirceEncoder, E]: F[GraphQLResponse[E]] =
+    GraphQLResponceCirce.graphQLResponseEncoder.asInstanceOf[F[GraphQLResponse[E]]]
+}
+
+private object GraphQLResponceCirce {
+  import io.circe._
+  import io.circe.syntax._
+  val graphQLResponseEncoder: Encoder[GraphQLResponse[CalibanError]] = Encoder
+    .instance[GraphQLResponse[CalibanError]](
+      response =>
+        Json.obj(
+          "data"   -> response.data.asJson,
+          "errors" -> Json.fromValues(response.errors.map(err => Json.fromString(err.toString)))
+        )
+    )
+}
