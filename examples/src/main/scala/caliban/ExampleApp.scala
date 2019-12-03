1b4a021355b778d93105cd785648c21f5b3cf58e
fokot <frantisek.kocun@gmail.com>
2019-12-03 11:36:21 +0100

    graphiql with StaticFile.fromResource

--- examples/src/main/scala/caliban/ExampleApp.scala
+++ examples/src/main/scala/caliban/ExampleApp.scala
@@ -4,8 +4,9 @@ import caliban.ExampleData._
 import caliban.GraphQL._
 import caliban.schema.Annotations.{GQLDeprecated, GQLDescription}
 import caliban.schema.GenericSchema
-import cats.data.{Kleisli, OptionT}
-import org.http4s.{EntityEncoder, HttpApp, HttpRoutes, Response, Status}
+import cats.data.Kleisli
+import cats.effect.Blocker
+import org.http4s.StaticFile
 import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
@@ -16,8 +17,6 @@ import zio.clock.Clock
 import zio.console.{Console, putStrLn}
 import zio.interop.catz._
 import zio.stream.ZStream
-
-import scala.io.Source
 
 object ExampleApp extends CatsApp with GenericSchema[Console with Clock] {
 
@@ -30,16 +29,12 @@ object ExampleApp extends CatsApp with GenericSchema[Console with Clock] {
   case class Mutations(deleteCharacter: CharacterArgs => URIO[Console, Boolean])
   case class Subscriptions(characterDeleted: ZStream[Console, Nothing, String])
 
-  type ExampleTask[A] = RIO[Console with Clock with Blocking, A]
+  type ExampleTask[A] = RIO[Console with Clock, A]
 
-  implicit val roleSchema           = gen[Role]
-  implicit val characterSchema      = gen[Character]
-  implicit val characterArgsSchema  = gen[CharacterArgs]
+  implicit val roleSchema = gen[Role]
+  implicit val characterSchema = gen[Character]
+  implicit val characterArgsSchema = gen[CharacterArgs]
   implicit val charactersArgsSchema = gen[CharactersArgs]
-
-  def staticResource(path: String): ExampleTask[Response[ExampleTask]] =
-    blocking.effectBlocking(Source.fromResource(path).getLines().mkString("\n"))
-      .map(content => Response[ExampleTask](Status.Ok, body = EntityEncoder[ExampleTask, String](EntityEncoder.stringEncoder).toEntity(content).body))
 
   override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
     (for {
@@ -54,17 +49,23 @@ object ExampleApp extends CatsApp with GenericSchema[Console with Clock] {
           Subscriptions(service.deletedEvents)
         )
       )
+      blocker <- ZIO
+        .accessM[Blocking](_.blocking.blockingExecutor.map(_.asEC))
+        .map(Blocker.liftExecutionContext)
       _ <- BlazeServerBuilder[ExampleTask]
-            .bindHttp(8088, "localhost")
-            .withHttpApp(
-              Router[ExampleTask](
-                "/api/graphql" -> CORS(Http4sAdapter.makeRestService(interpreter)),
-                "/ws/graphql"  -> CORS(Http4sAdapter.makeWebSocketService(interpreter)),
-                "/graphiql" -> Kleisli.liftF(OptionT.liftF(staticResource("graphiql.html")))
-              ).orNotFound
-            )
-            .resource
-            .toManaged
-            .useForever
+        .bindHttp(8088, "localhost")
+        .withHttpApp(
+          Router[ExampleTask](
+            "/api/graphql" -> CORS(Http4sAdapter.makeRestService(interpreter)),
+            "/ws/graphql" -> CORS(
+              Http4sAdapter.makeWebSocketService(interpreter)
+            ),
+            "/graphiql" -> Kleisli
+              .liftF(StaticFile.fromResource("/graphiql.html", blocker, None))
+          ).orNotFound
+        )
+        .resource
+        .toManaged
+        .useForever
     } yield 0).catchAll(err => putStrLn(err.toString).as(1))
 }
