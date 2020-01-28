ac52b2b8e89223e290b3bee9bef359f2b54eb631
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2020-01-28 20:17:45 +0900

    Support for circe json scalar (#176)
    * Support for circe json scalar
    
    * Fix test
    
    * Remove duplication
    
    * Fix mistake + add a test

--- core/src/main/scala/caliban/interop/circe/circe.scala
+++ core/src/main/scala/caliban/interop/circe/circe.scala
@@ -1,6 +1,13 @@
 package caliban.interop.circe
 
+import caliban.introspection.adt.__Type
+import caliban.schema.Step.QueryStep
+import caliban.schema.Types.makeScalar
+import caliban.schema.{ ArgBuilder, PureStep, Schema, Step }
+import caliban.{ InputValue, ResponseValue }
 import io.circe._
+import zio.ZIO
+import zquery.ZQuery
 
 /**
  * This class is an implementation of the pattern described in https://blog.7mind.io/no-more-orphans.html
@@ -18,4 +25,13 @@ private[caliban] object IsCirceEncoder {
 private[caliban] trait IsCirceDecoder[F[_]]
 private[caliban] object IsCirceDecoder {
   implicit val isCirceDecoder: IsCirceDecoder[Decoder] = null
+}
+
+object json {
+  implicit val jsonSchema: Schema[Any, Json] = new Schema[Any, Json] {
+    override def toType(isInput: Boolean): __Type = makeScalar("Json")
+    override def resolve(value: Json): Step[Any] =
+      QueryStep(ZQuery.fromEffect(ZIO.fromEither(Decoder[ResponseValue].decodeJson(value))).map(PureStep))
+  }
+  implicit val jsonArgBuilder: ArgBuilder[Json] = (input: InputValue) => Right(Encoder[InputValue].apply(input))
 }
