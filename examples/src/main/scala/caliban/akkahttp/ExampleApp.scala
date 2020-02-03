4441800353dcbe8d90bb3b7c73414ec4f2f04417
phderome <phderome@gmail.com>
2020-02-02 22:52:27 -0500

    some potential proposal of refactoring of Http4sAdapter though there is no apparent/intentional functional change
    Minor progress on ws solution for AkkaHttpAdapter

--- examples/src/main/scala/caliban/akkahttp/ExampleApp.scala
+++ examples/src/main/scala/caliban/akkahttp/ExampleApp.scala
@@ -3,20 +3,23 @@ package caliban.akkahttp
 import scala.io.StdIn
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import zio.{ DefaultRuntime, URIO }
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
+import akka.stream.Materializer
 import caliban.ExampleData.{ sampleCharacters, Character, CharacterArgs, CharactersArgs, Role }
 import caliban.GraphQL.graphQL
 import caliban.schema.Annotations.{ GQLDeprecated, GQLDescription }
 import caliban.schema.GenericSchema
-import caliban.{ AkkaHttpAdapter, ExampleService, RootResolver }
+import caliban.{ AkkaHttpAdapter, ExampleService, GraphQL, RootResolver }
 import zio.clock.Clock
 import zio.console.Console
 import zio.stream.ZStream
-import zio.{ DefaultRuntime, URIO }
 
 object ExampleApp extends App with GenericSchema[Console with Clock] {
 
   implicit val system           = ActorSystem()
+  implicit val materializer     = Materializer.matFromSystem
   implicit val executionContext = system.dispatcher
   implicit val defaultRuntime   = new DefaultRuntime {}
 
@@ -24,6 +27,18 @@ object ExampleApp extends App with GenericSchema[Console with Clock] {
   implicit val characterSchema      = gen[Character]
   implicit val characterArgsSchema  = gen[CharacterArgs]
   implicit val charactersArgsSchema = gen[CharactersArgs]
+
+  def makeApi(service: ExampleService): GraphQL[Console with Clock] =
+    graphQL(
+      RootResolver(
+        Queries(
+          args => service.getCharacters(args.origin),
+          args => service.findCharacter(args.name)
+        ),
+        Mutations(args => service.deleteCharacter(args.name)),
+        Subscriptions(service.deletedEvents)
+      )
+    )
 
   case class Queries(
     @GQLDescription("Return all characters from a given origin")
@@ -34,21 +49,17 @@ object ExampleApp extends App with GenericSchema[Console with Clock] {
   case class Mutations(deleteCharacter: CharacterArgs => URIO[Console, Boolean])
   case class Subscriptions(characterDeleted: ZStream[Console, Nothing, String])
 
-  val interpreter = 
+  val interpreter =
     ExampleService
       .make(sampleCharacters)
-      .map(service => {
-        graphQL(
-          RootResolver(
-            Queries(
-              args => service.getCharacters(args.origin),
-              args => service.findCharacter(args.name)
-            ),
-            Mutations(args => service.deleteCharacter(args.name)),
-            Subscriptions(service.deletedEvents)
-          )
-        ).interpreter
-      })
+      .map(service => makeApi(service).interpreter)
+
+  val apiRoute: Route = AkkaHttpAdapter.makeHttpServiceM(interpreter)
+  val wsApiRoute = {
+    // when do we create subscriptions? When do we make it effectful/unsafe?
+    //  val subscriptions: UIO[Ref[Map[String, Fiber[Throwable, Unit]]]] = Ref.make(Map.empty[String, Fiber[Throwable, Unit]])
+    AkkaHttpAdapter.makeWebSocketServiceM(interpreter)
+  }
 
   /**
    * curl -X POST \
@@ -59,13 +70,16 @@ object ExampleApp extends App with GenericSchema[Console with Clock] {
    * "query": "query { characters { name }}"
    * }'
    */
-  val route =
+  val route: Route =
     path("api" / "graphql") {
-      AkkaHttpAdapter.makeHttpServiceM(interpreter)
+      apiRoute // do not call makeHttpServiceM here, or it will be recreated at every call
+    } ~ path("ws" / "graphql") {
+      wsApiRoute
     } ~ path("graphiql") {
       getFromResource("graphiql.html")
     }
 
+  // convention is normally 8080. Keep at 8088?
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8088)
   println(s"Server online at http://localhost:8088/\nPress RETURN to stop...")
   StdIn.readLine()
