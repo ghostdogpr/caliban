5968c7c65d576ed256addaca5e77eaef68321e96
Pierre Ricadat <ghostdogpr@gmail.com>
2020-02-17 23:02:50 +0900

    Clean up map on stop

--- akka-http/src/main/scala/caliban/AkkaHttpAdapter.scala
+++ akka-http/src/main/scala/caliban/AkkaHttpAdapter.scala
@@ -144,9 +144,9 @@ object AkkaHttpAdapter extends FailFastCirceSupport {
                       }
                     case "stop" =>
                       val id = msg.hcursor.downField("id").success.flatMap(_.value.asString).getOrElse("")
-                      subscriptions.get.flatMap(
-                        map => IO.whenCase(map.get(id)) { case Some(fiber) => fiber.interrupt }
-                      )
+                      subscriptions
+                        .modify(map => (map.get(id), map - id))
+                        .flatMap(fiber => IO.whenCase(fiber) { case Some(fiber) => fiber.interrupt })
                   }
             } yield ()
             runtime.unsafeRun(io)
