5c35689b594556516c35bd2ecfa3565bd7fc093c
phderome <phderome@gmail.com>
2020-02-17 20:55:50 -0500

    refine Akka ws solution to be more exhaustive in handling client inputs

--- akka-http/src/main/scala/caliban/AkkaHttpAdapter.scala
+++ akka-http/src/main/scala/caliban/AkkaHttpAdapter.scala
@@ -1,8 +1,9 @@
 package caliban
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext }
 import akka.http.scaladsl.model.MediaTypes.`application/json`
-import akka.http.scaladsl.model.ws.{ Message, TextMessage }
+import akka.http.scaladsl.model.ws.{ BinaryMessage, Message, TextMessage }
+import akka.http.scaladsl.model.ws.BinaryMessage.Streamed
 import akka.http.scaladsl.model.{ HttpEntity, HttpResponse, StatusCodes }
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.{ Route, StandardRoute }
@@ -15,7 +16,7 @@ import io.circe.Decoder.Result
 import io.circe.Json
 import io.circe.parser._
 import io.circe.syntax._
-import zio.{ Fiber, IO, Ref, Runtime, Task, URIO }
+import zio.{ Fiber, IO, RIO, Ref, Runtime, Task, URIO }
 
 object AkkaHttpAdapter extends FailFastCirceSupport {
 
@@ -87,69 +88,82 @@ object AkkaHttpAdapter extends FailFastCirceSupport {
                 )
                 .noSpaces
             )
-          )
+        )
       )
+
+    def processMessage(queue: SourceQueueWithComplete[Message],
+                       subscriptions: Ref[Map[String, Fiber[Throwable, Unit]]],
+                       text: String): RIO[R, Unit] =
+      for {
+        msg     <- Task.fromEither(decode[Json](text))
+        msgType = msg.hcursor.downField("type").success.flatMap(_.value.asString).getOrElse("")
+        _ <- IO.whenCase(msgType) {
+              case "connection_init" =>
+                IO.fromFuture(_ => queue.offer(TextMessage("""{"type":"connection_ack"}""")))
+              case "connection_terminate" =>
+                IO.effect(queue.complete())
+              case "start" =>
+                val payload = msg.hcursor.downField("payload")
+                val id      = msg.hcursor.downField("id").success.flatMap(_.value.asString).getOrElse("")
+                Task.whenCase(payload.downField("query").success.flatMap(_.value.asString)) {
+                  case Some(query) =>
+                    val operationName = payload.downField("operationName").success.flatMap(_.value.asString)
+                    (for {
+                      result <- execute(interpreter, GraphQLRequest(query, operationName, None))
+                      _ <- result.data match {
+                            case ObjectValue((fieldName, StreamValue(stream)) :: Nil) =>
+                              stream.foreach { item =>
+                                sendMessage(queue, id, ObjectValue(List(fieldName -> item)), result.errors)
+                              }.fork.flatMap(fiber => subscriptions.update(_.updated(id, fiber)))
+                            case other =>
+                              sendMessage(queue, id, other, result.errors) *> IO.fromFuture(
+                                _ => queue.offer(TextMessage(s"""{"type":"complete","id":"$id"}"""))
+                              )
+                          }
+                    } yield ()).catchAll(
+                      error =>
+                        IO.fromFuture(
+                          _ =>
+                            queue.offer(
+                              TextMessage(
+                                Json
+                                  .obj(
+                                    "id"      -> Json.fromString(id),
+                                    "type"    -> Json.fromString("complete"),
+                                    "payload" -> Json.fromString(error.toString)
+                                  )
+                                  .noSpaces
+                              )
+                          )
+                      )
+                    )
+                }
+              case "stop" =>
+                val id = msg.hcursor.downField("id").success.flatMap(_.value.asString).getOrElse("")
+                subscriptions
+                  .modify(map => (map.get(id), map - id))
+                  .flatMap(fiber => IO.whenCase(fiber) { case Some(fiber) => fiber.interrupt })
+            }
+      } yield ()
 
     import akka.http.scaladsl.server.Directives._
 
     get {
       extractUpgradeToWebSocket { upgrade =>
         val (queue, source) = Source.queue[Message](0, OverflowStrategy.fail).preMaterialize()
-        val subscriptions   = runtime.unsafeRun(Ref.make(Map.empty[String, Fiber[Throwable, Unit]]))
+        val subscriptions: Ref[Map[String, Fiber[Throwable, Unit]]] =
+          runtime.unsafeRun(Ref.make(Map.empty[String, Fiber[Throwable, Unit]]))
+
         val sink = Sink.foreach[Message] {
-          case TextMessage.Strict(text) =>
-            val io = for {
-              msg     <- Task.fromEither(decode[Json](text))
-              msgType = msg.hcursor.downField("type").success.flatMap(_.value.asString).getOrElse("")
-              _ <- IO.whenCase(msgType) {
-                    case "connection_init" =>
-                      IO.fromFuture(_ => queue.offer(TextMessage("""{"type":"connection_ack"}""")))
-                    case "connection_terminate" =>
-                      IO.effect(queue.complete())
-                    case "start" =>
-                      val payload = msg.hcursor.downField("payload")
-                      val id      = msg.hcursor.downField("id").success.flatMap(_.value.asString).getOrElse("")
-                      Task.whenCase(payload.downField("query").success.flatMap(_.value.asString)) {
-                        case Some(query) =>
-                          val operationName = payload.downField("operationName").success.flatMap(_.value.asString)
-                          (for {
-                            result <- execute(interpreter, GraphQLRequest(query, operationName, None))
-                            _ <- result.data match {
-                                  case ObjectValue((fieldName, StreamValue(stream)) :: Nil) =>
-                                    stream.foreach { item =>
-                                      sendMessage(queue, id, ObjectValue(List(fieldName -> item)), result.errors)
-                                    }.fork.flatMap(fiber => subscriptions.update(_.updated(id, fiber)))
-                                  case other =>
-                                    sendMessage(queue, id, other, result.errors) *> IO.fromFuture(
-                                      _ => queue.offer(TextMessage(s"""{"type":"complete","id":"$id"}"""))
-                                    )
-                                }
-                          } yield ()).catchAll(
-                            error =>
-                              IO.fromFuture(
-                                _ =>
-                                  queue.offer(
-                                    TextMessage(
-                                      Json
-                                        .obj(
-                                          "id"      -> Json.fromString(id),
-                                          "type"    -> Json.fromString("complete"),
-                                          "payload" -> Json.fromString(error.toString)
-                                        )
-                                        .noSpaces
-                                    )
-                                  )
-                              )
-                          )
-                      }
-                    case "stop" =>
-                      val id = msg.hcursor.downField("id").success.flatMap(_.value.asString).getOrElse("")
-                      subscriptions
-                        .modify(map => (map.get(id), map - id))
-                        .flatMap(fiber => IO.whenCase(fiber) { case Some(fiber) => fiber.interrupt })
-                  }
-            } yield ()
-            runtime.unsafeRun(io)
+          case TextMessage.Strict(text) => runtime.unsafeRun(processMessage(queue, subscriptions, text))
+          case Streamed(textStream) =>
+            textStream
+              .runFold("")(_ + _)
+              .map(message => runtime.unsafeRun(processMessage(queue, subscriptions, message)))
+            ()
+          case bm: BinaryMessage =>
+            bm.dataStream.runWith(Sink.ignore)
+            ()
           case _ => ()
         }
 
