30a2e6b27763f9dfa92f61ebbf3f750303f61856
Haemin Yoo <haemin@zzz.pe.kr>
2020-02-23 17:58:54 +0900

    Make runtime parametric to environment type

--- play/src/main/scala/caliban/CalibanController.scala
+++ play/src/main/scala/caliban/CalibanController.scala
@@ -3,17 +3,20 @@ package caliban
 import caliban.PlayJson._
 import javax.inject.Inject
 import play.api.mvc.{ AbstractController, Action, ControllerComponents }
-import zio.DefaultRuntime
+import zio.Runtime
 
-abstract class CalibanController @Inject() (cc: ControllerComponents)
-    extends AbstractController(cc)
-    with DefaultRuntime {
+class CalibanController[-R] @Inject() (cc: ControllerComponents, runtime: Runtime[R])
+    extends AbstractController(cc) {
 
   def action[E](interpreter: GraphQLInterpreter[Any, E]): Action[GraphQLRequest] =
     Action.async(parse.json[GraphQLRequest]) { req =>
-      unsafeRunToFuture(
-        interpreter.execute(req.body.query, req.body.operationName, req.body.variables.getOrElse(Map.empty)).map(Ok(_))
-      ).future
+      runtime
+        .unsafeRunToFuture(
+          interpreter
+            .execute(req.body.query, req.body.operationName, req.body.variables.getOrElse(Map.empty))
+            .map(Ok(_))
+        )
+        .future
     }
 
 }
