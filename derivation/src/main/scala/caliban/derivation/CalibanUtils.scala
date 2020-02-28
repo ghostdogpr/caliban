1cd9f3ef9b7d7346630729b1595988509048f451
Ivan Motyashov <argson@gmail.com>
2020-02-27 18:59:44 -0800

    Fix handling of annotations and correctly handle the forInput case

--- derivation/src/main/scala/caliban/derivation/CalibanUtils.scala
+++ derivation/src/main/scala/caliban/derivation/CalibanUtils.scala
@@ -1,25 +1,32 @@
 package caliban.derivation
 
-import caliban.schema.Annotations.{ GQLDeprecated, GQLDescription, GQLName }
+import caliban.schema.Annotations.{ GQLDeprecated, GQLDescription, GQLDirective, GQLInputName, GQLName }
 
 /**
  * Caliban-specific macro utilities
  */
 trait CalibanUtils extends MacroUtils {
+
   import c.universe._
 
   protected case class GraphQLInfo(
     name: Tree,
+    inputName: Tree,
     description: Tree,
-    deprecationReason: Tree
+    deprecationReason: Tree,
+    directives: Tree
   ) {
     def isDeprecated: Tree = q"$deprecationReason.isDefined"
+
+    def directivesOpt: Tree = q"Some($directives).filterNot(_.isEmpty)"
   }
 
   protected object GraphQLInfo {
     private val GQLNameType        = OneArgumentAnnotation[GQLName]
     private val GQLDescriptionType = OneArgumentAnnotation[GQLDescription]
     private val GQLDeprecatedType  = OneArgumentAnnotation[GQLDeprecated]
+    private val GQLDirectiveType   = OneArgumentAnnotation[GQLDirective]
+    private val GQLInputNameType   = OneArgumentAnnotation[GQLInputName]
 
     private def getName(s: Symbol): Tree = {
       val default =
@@ -42,28 +49,49 @@ trait CalibanUtils extends MacroUtils {
         case GQLDeprecatedType(reason) => q"Some($reason)"
       }.getOrElse(q"None")
 
+    private def getDirectives(s: Symbol): Tree = {
+      val dirs = s.scalaAnnotations.collect {
+        case GQLDirectiveType(dir) => dir
+      }
+
+      q"List(..$dirs)"
+    }
+
+    private def getInputName(s: Symbol): Tree =
+      s.scalaAnnotations.collectFirst {
+        case GQLInputNameType(name) => name
+      }.getOrElse(q"${refs.customizeInputTypeName}(${getName(s)})")
+
     def apply(s: Symbol): GraphQLInfo = {
       assert(s != NoSymbol)
 
-      GraphQLInfo(getName(s), getDescription(s), getDeprecation(s))
+      GraphQLInfo(getName(s), getInputName(s), getDescription(s), getDeprecation(s), getDirectives(s))
     }
   }
 
-  protected object Ref {
-    val __Field: RefTree      = companionRef[caliban.introspection.adt.__Field]
-    val __InputValue: RefTree = companionRef[caliban.introspection.adt.__InputValue]
-    val objectSchema: RefTree = companionRef[caliban.schema.Schema[_, _]].select("objectSchema")
-    val makeNonNull: RefTree  = companionRef[caliban.schema.Types.type].select("makeNonNull")
-    val FunctionStep: RefTree = companionRef[caliban.schema.Step.FunctionStep[_]]
-    val QueryStep: RefTree    = companionRef[caliban.schema.Step.QueryStep[_]]
-    val ZQuery: RefTree       = companionRef[zquery.ZQuery[_, _, _]]
+  protected object refs {
+    val __Field: RefTree                = companionRef[caliban.introspection.adt.__Field]
+    val __InputValue: RefTree           = companionRef[caliban.introspection.adt.__InputValue]
+    val customizeInputTypeName: RefTree = companionRef[caliban.schema.Schema[_, _]].select("customizeInputTypeName")
+    val makeNonNull: RefTree            = companionRef[caliban.schema.Types.type].select("makeNonNull")
+    val makeObject: RefTree             = companionRef[caliban.schema.Types.type].select("makeObject")
+    val makeInputObject: RefTree        = companionRef[caliban.schema.Types.type].select("makeInputObject")
+    val FunctionStep: RefTree           = companionRef[caliban.schema.Step.FunctionStep[_]]
+    val ObjectStep: RefTree             = companionRef[caliban.schema.Step.ObjectStep[_]]
+    val QueryStep: RefTree              = companionRef[caliban.schema.Step.QueryStep[_]]
+    val ZQuery: RefTree                 = companionRef[zquery.ZQuery[_, _, _]]
+  }
+
+  protected object typeRefs {
+    val __Type: Type = typeOf[caliban.introspection.adt.__Type]
+    val Any: Type    = typeOf[Any]
   }
 
   protected object Schema extends HKType {
     private val sym: Symbol = symbolOf[caliban.schema.Schema[_, _]]
-    private val any: Type   = typeOf[Any]
 
-    def apply(tpe: Type): Type = internal.typeRef(NoPrefix, sym, List(any, tpe))
+    def apply(tpe: Type): Type = internal.typeRef(NoPrefix, sym, List(typeRefs.Any, tpe))
+
     def unapply(tpe: Type): Option[Type] =
       tpe match {
         case TypeRef(_, `sym`, List(_, x)) => Some(x)
@@ -75,6 +103,7 @@ trait CalibanUtils extends MacroUtils {
     private val sym: Symbol = symbolOf[caliban.schema.ArgBuilder[_]]
 
     def apply(tpe: Type): Type = internal.typeRef(NoPrefix, sym, List(tpe))
+
     def unapply(tpe: Type): Option[Type] =
       tpe match {
         case TypeRef(_, `sym`, List(x)) => Some(x)
@@ -82,7 +111,19 @@ trait CalibanUtils extends MacroUtils {
       }
   }
 
-  protected def mkCalibanType(schema: Tree, isInput: Boolean = false): Tree = {
+  protected object Step extends HKType {
+    private val sym: Symbol = symbolOf[caliban.schema.Step[_]]
+
+    def apply(tpe: Type): Type = internal.typeRef(NoPrefix, sym, List(tpe))
+
+    def unapply(tpe: Type): Option[Type] =
+      tpe match {
+        case TypeRef(_, `sym`, List(x)) => Some(x)
+        case _                          => None
+      }
+  }
+
+  protected def mkCalibanType(schema: Tree, isInput: Boolean): Tree = {
     val isInputTree = mkConst(isInput)
 
     q"""
@@ -90,7 +131,7 @@ trait CalibanUtils extends MacroUtils {
         if ($schema.optional)
           $schema.toType($isInputTree)
         else
-          ${Ref.makeNonNull}($schema.toType($isInputTree))
+          ${refs.makeNonNull}($schema.toType($isInputTree))
     """
   }
 }
--- derivation/src/main/scala/caliban/derivation/DerivationMacros.scala
+++ derivation/src/main/scala/caliban/derivation/DerivationMacros.scala
@@ -12,7 +12,10 @@ import scala.reflect.macros.blackbox
 class DerivationMacros(val c: blackbox.Context) extends CalibanUtils {
   import c.universe._
 
-  private case class DeriveMember(parent: Type, sym: TermSymbol) {
+  private case class DeriveMember(sym: TermSymbol) {
+    // n.b. To remain consistent with the magnolia derivation, we ignore the GQLName and GQLInputName annotations
+    // on fields, methods, and parameters. Though I don't see a reason why we cannot use them for these cases as well.
+
     // Only accept methods that have a single (non-implicit) parameter list
     assert(!sym.isMethod || sym.asMethod.paramLists.count(nonImplicit) <= 1)
     // Only accept methods that do not have type parameters
@@ -28,29 +31,27 @@ class DerivationMacros(val c: blackbox.Context) extends CalibanUtils {
 
     private val fieldName: TermName = sym.name.normalize
 
-    private def deriveParam(param: Symbol): Tree = {
-      // TODO: Figure out how to cleanly extract the default argument, if any
+    private val fieldNameStr: Tree = mkConst(sym.name.normalizeString)
 
-      val info = GraphQLInfo(param)
-
-      Schema.summon(param.typeSignature) { paramSchema =>
-        q"""
-          ${Ref.__InputValue}(
-            ${info.name},
-            ${info.description},
-            ${mkCalibanType(Ident(paramSchema))},
-            None
-          )
-        """
-      }
+    // TODO: Figure out how to cleanly extract the default argument, if any
+    def deriveParam: Tree = Schema.summon(scalaType) { paramSchema =>
+      q"""
+        ${refs.__InputValue}(
+          $fieldNameStr,
+          ${info.description},
+          ${mkCalibanType(Ident(paramSchema), isInput = true)},
+          None,
+          ${info.directivesOpt}
+        )
+      """
     }
 
-    private def deriveField(schema: TermName): Tree = {
+    def deriveField: Tree = Schema.summon(scalaType) { schema =>
       val args =
         params.filterNot(_.isEmpty) match {
           case Some(ps) =>
             // Non-empty list of parameters
-            val argTrees = ps.map(deriveParam)
+            val argTrees = ps.map(p => DeriveMember(p.asTerm).deriveParam)
             q"List(..$argTrees)"
 
           case None =>
@@ -58,80 +59,151 @@ class DerivationMacros(val c: blackbox.Context) extends CalibanUtils {
             q"Nil"
         }
 
-      val tpe = mkCalibanType(Ident(schema))
+      val tpe = mkCalibanType(Ident(schema), isInput = false)
 
       q"""
-        ${Ref.__Field}(
-          ${info.name},
+        ${refs.__Field}(
+          $fieldNameStr,
           ${info.description},
           $args,
           $tpe,
           ${info.isDeprecated},
-          ${info.deprecationReason}
+          ${info.deprecationReason},
+          ${info.directivesOpt}
         )
       """
     }
 
-    private def deriveStep(schema: TermName): Tree = {
+    def deriveStep(parent: Type, resolveValue: TermName): Tree = Schema.summon(scalaType) { schema =>
       val emptyParamList: Boolean =
         params.exists(_.isEmpty)
 
-      // Generate a function from the enclosing case class instance to a Step for resolving this field
-      mkFunction(parent) { parentVar =>
-        params.filterNot(_.isEmpty) match {
-          case Some(ps) =>
-            // Non-empty list of parameters
+      params.filterNot(_.isEmpty) match {
+        case Some(ps) =>
+          // Non-empty list of parameters
 
-            // Generate a function from the InputValue's received to a Step for resolving the method call
-            val functionStep = mkFunction() { args =>
-              val paramNames = ps.map(p => c.freshName(p.name))
+          // Generate a function from the InputValue's received to a Step for resolving the method call
+          val functionStep = mkFunction() { args =>
+            val paramNames = ps.map(p => c.freshName(p.name))
 
-              val builtArgs =
-                ps.zip(paramNames).map {
-                  case (p, name) =>
-                    val paramInfo = GraphQLInfo(p)
+            val builtArgs =
+              ps.zip(paramNames).map {
+                case (p, name) =>
+                  val paramName = p.nameStringLit
+                  val rhs = ArgBuilder.summon(p.typeSignature)(bld => q"$bld.build($args($paramName))")
 
-                    val rhs = ArgBuilder.summon(p.typeSignature)(bld => q"$bld.build($args(${paramInfo.name}))")
+                  fq"$name <- $rhs"
+              }
 
-                    fq"$name <- $rhs"
-                }
+            val forExpr = q"for (..$builtArgs) yield $resolveValue.$fieldName(..$paramNames)"
 
-              val forExpr = q"for (..$builtArgs) yield $parentVar.$fieldName(..$paramNames)"
+            q"""
+              $forExpr match {
+                case Left(error) => ${refs.QueryStep}(${refs.ZQuery}.fail(error))
+                case Right(value) => $schema.resolve(value)
+              }
+            """
+          }
 
-              q"""
-                $forExpr match {
-                  case Left(error) => ${Ref.QueryStep}(${Ref.ZQuery}.fail(error))
-                  case Right(value) => $schema.resolve(value)
-                }
-              """
-            }
+          q"${refs.FunctionStep}($functionStep)"
 
-            q"${Ref.FunctionStep}($functionStep)"
+        case None =>
+          // Parameterless or empty parameter list
+          val invoke =
+            if (emptyParamList) q"$resolveValue.$fieldName()"
+            else q"$resolveValue.$fieldName"
 
-          case None =>
-            // Parameterless or empty parameter list
-            val invoke =
-              if (emptyParamList) q"$parentVar.$fieldName()"
-              else q"$parentVar.$fieldName"
-
-            q"$schema.resolve($invoke)"
-        }
+          q"$schema.resolve($invoke)"
       }
     }
 
-    def derive: Tree =
-      Schema.summon(scalaType)(schema => q"(${deriveField(schema)}, ${deriveStep(schema)})")
+    def deriveStepWithName(parent: Type, resolveValue: TermName): Tree =
+      q"($fieldNameStr, ${deriveStep(parent, resolveValue)})"
   }
 
   private def nonImplicit(params: List[Symbol]): Boolean =
     !params.exists(_.isImplicit)
 
-  private def canDerive(sym: TermSymbol): Boolean =
+  private def canDeriveOutput(sym: TermSymbol): Boolean =
     (!sym.isMethod || sym.asMethod.typeParams.isEmpty) &&
       sym.isPublic &&
       !sym.isConstructor &&
-      !sym.isSynthetic &&
-      !hasAnnotation[GQLExclude](sym)
+      !sym.isSynthetic
+
+  private def canDeriveInput(sym: TermSymbol): Boolean =
+    sym.isPublic && sym.isCaseAccessor
+
+  /**
+   * Retrieve the underlying symbol for the accessor that holds the annotations
+   *
+   * For a case class parameter, this will be the corresponding constructor parameter,
+   * and for regular vals/vars it will be the underlying val/var symbol.
+   */
+  private def underlyingField(tpe: Type, ms: MethodSymbol): TermSymbol =
+    if (ms.isCaseAccessor) {
+      val cons = tpe.decls.collectFirst {
+        case ms: MethodSymbol if ms.isPrimaryConstructor => ms
+      }.get
+
+      cons.paramLists.head.find(_.name == ms.name).get.asTerm
+    } else if (!ms.isGetter) ms
+    else {
+      tpe.decls.collectFirst {
+        case sym: TermSymbol if !sym.isMethod && sym.name.normalizeString == ms.nameString => sym
+      }.get
+    }
+
+  private def inputFields(tpe: Type): List[TermSymbol] =
+    tpe.decls.collect { case ms: MethodSymbol => ms }
+      .filter(canDeriveInput)
+      .map(underlyingField(tpe, _))
+      .toList
+
+  private def outputFields(tpe: Type): List[TermSymbol] =
+    tpe.decls.collect { case ms: MethodSymbol => ms }
+      .filter(canDeriveOutput)
+      .map(underlyingField(tpe, _))
+      .filterNot(hasAnnotation[GQLExclude])
+      .toList
+
+  private def deriveInput(info: GraphQLInfo, inputs: List[TermSymbol]): Tree = {
+    val fields =
+      inputs.map(i => DeriveMember(i).deriveParam)
+
+    q"""
+      ${refs.makeInputObject}(
+        Some(${info.inputName}),
+        ${info.description},
+        List(..$fields)
+      )
+    """
+  }
+
+  private def deriveOutput(info: GraphQLInfo, outputs: List[TermSymbol]): Tree = {
+    val fields =
+      outputs.map(o => DeriveMember(o).deriveField)
+
+    q"""
+      ${refs.makeObject}(
+        Some(${info.name}),
+        ${info.description},
+        List(..$fields),
+        ${info.directives}
+      )
+    """
+  }
+
+  private def deriveStep(parent: Type, info: GraphQLInfo, resolveValue: TermName, outputs: List[TermSymbol]): Tree = {
+    val fields =
+      outputs.map(o => DeriveMember(o).deriveStepWithName(parent, resolveValue))
+
+    q"""
+      ${refs.ObjectStep}(
+        ${info.name},
+        Map(..$fields)
+      )
+    """
+  }
 
   def deriveSchema[T](implicit wtt: WeakTypeTag[T]): Tree = {
     val tpe = wtt.tpe
@@ -140,21 +212,29 @@ class DerivationMacros(val c: blackbox.Context) extends CalibanUtils {
       c.abort(c.enclosingPosition, s"Only concrete classes can currently be derived with the `deriveSchema` macro")
     }
 
-    val candidates = tpe.decls.collect {
-      case ms: MethodSymbol =>
-        if (ms.isGetter) ms
-        else {
-          tpe.decls.collectFirst {
-            case sym: TermSymbol if !sym.isMethod && sym.name.normalizeString == ms.nameString => sym
-          }.get
-        }
-    }.filter(canDerive)
+    val inputs  = inputFields(tpe)
+    val outputs = outputFields(tpe)
 
-    val info   = GraphQLInfo(tpe.typeSymbol)
-    val fields = candidates.map(DeriveMember(tpe, _).derive)
+    val resolveValue = TermName(c.freshName())
+
+    val info = GraphQLInfo(tpe.typeSymbol)
+    val toType =
+      q"""
+        override def toType(isInput: Boolean = false): ${typeRefs.__Type} =
+          if (isInput) ${deriveInput(info, inputs)}
+          else ${deriveOutput(info, outputs)}
+      """
+    val resolve =
+      q"""
+         override def resolve($resolveValue: $tpe): ${Step(typeRefs.Any)} =
+           ${deriveStep(tpe, info, resolveValue, outputs)}
+      """
 
     q"""
-      ${Ref.objectSchema}(${info.name}, ${info.description}, List(..$fields))
+      new ${Schema(tpe)} {
+        $toType
+        $resolve
+      }
     """
   }
 }
