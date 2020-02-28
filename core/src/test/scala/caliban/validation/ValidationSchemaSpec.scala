2f25426d81dd2eb6f40c2ed20e786b39ef429e99
Tobias Pfeifer <to.pfeifer@web.de>
2020-02-28 23:44:42 +0100

    add tests

--- core/src/test/scala/caliban/validation/ValidationSchemaSpec.scala
+++ core/src/test/scala/caliban/validation/ValidationSchemaSpec.scala
@@ -13,7 +13,7 @@ object ValidationSchemaSpec
       def check(gql: GraphQL[Any], expectedMessage: String): IO[ValidationError, TestResult] =
         assertM(gql.interpreter.run, fails[ValidationError](hasField("msg", _.msg, equalTo(expectedMessage))))
 
-      suite("ValidationSchemaSpec")({
+      suite("ValidationSchemaSpec")(
         suite("InputObjects")(
           testM("name can't start with '__'") {
             check(
@@ -27,6 +27,32 @@ object ValidationSchemaSpec
               "UnionInput is of kind UNION, must be an InputType"
             )
           }
+        ),
+        suite("Interface")(
+          testM("must define one or more fields") {
+            check(
+              graphQL(resolverEmptyInferface),
+              "message"
+            )
+          },
+          testM("field name can't start with '__'") {
+            check(
+              graphQL(resolverInferfaceWrongFieldName),
+              "A field in Interface can't start with '__': __name"
+            )
+          },
+          testM("field argument name can't start with '__'") {
+            check(
+              graphQL(resolverInterfaceWrongArgumentName),
+              "A argument input value in Interface can't start with '__': __name"
+            )
+          },
+          testM("field argument can't be output type") {
+            check(
+              graphQL(resolverInterfaceWrongArgumentInputType),
+              "UnionInput is of kind UNION, must be an InputType"
+            )
+          }
         )
-      })
+      )
     })
