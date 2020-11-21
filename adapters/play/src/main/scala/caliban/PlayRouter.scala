cbd9326700509690f72ff5ceee5c4bc966a5b548
Jessen <4315281+jessenr@users.noreply.github.com>
2020-11-21 13:09:39 +0000

    use query param on post request

--- adapters/play/src/main/scala/caliban/PlayRouter.scala
+++ adapters/play/src/main/scala/caliban/PlayRouter.scala
@@ -2,7 +2,7 @@ package caliban
 
 import akka.stream.Materializer
 import caliban.PlayAdapter.RequestWrapper
-import play.api.mvc.{ ActionBuilder, AnyContent, ControllerComponents, PlayBodyParsers, Request, Results }
+import play.api.mvc._
 import play.api.routing.Router.Routes
 import play.api.routing.SimpleRouter
 import play.api.routing.sird._
@@ -30,7 +30,14 @@ case class PlayRouter[R, E](
   implicit val ec: ExecutionContext                              = controllerComponents.executionContext
 
   override def routes: Routes = {
-    case POST(p"/api/graphql") => makePostAction(interpreter, skipValidation, enableIntrospection)
+    case POST(
+        p"/api/graphql" ? q_o"query=$query" & q_o"variables=$variables" & q_o"operationName=$operation" & q_o"extensions=$extensions"
+        ) =>
+      query match {
+        case Some(_) =>
+          makeGetAction(interpreter, skipValidation, enableIntrospection)(query, variables, operation, extensions)
+        case None => makePostAction(interpreter, skipValidation, enableIntrospection)
+      }
     case GET(
         p"/api/graphql" ? q_o"query=$query" & q_o"variables=$variables" & q_o"operationName=$operation" & q_o"extensions=$extensions"
         ) if allowGETRequests =>
