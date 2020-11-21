321f13e03915173eb86465ff1bf3248c444d926f
Terrence Drozdowski <tdrozdowski@gmail.com>
2020-11-21 16:11:59 -0700

    Fix for Issue 603 (#640)
    * added getMessage for CalibanClientError classes; spec for CalibanClientError classes
    
    * updated per comments in PR
    
    * attemped fix for extra space in toString
    
    * attemped fix for extra space in toString
    
    Co-authored-by: terry <terry.drozdowski@icloud.com>

--- client/src/main/scala/caliban/client/CalibanClientError.scala
+++ client/src/main/scala/caliban/client/CalibanClientError.scala
@@ -3,7 +3,9 @@ package caliban.client
 /**
  * The base type for all Caliban Client errors.
  */
-sealed trait CalibanClientError extends Throwable with Product with Serializable
+sealed trait CalibanClientError extends Throwable with Product with Serializable {
+  override def getMessage: String = toString
+}
 
 object CalibanClientError {
 
@@ -11,26 +13,27 @@ object CalibanClientError {
    * An error while communicating with the backend (e.g. HTTP code 4xx or 5xx)
    */
   case class CommunicationError(msg: String, innerThrowable: Option[Throwable] = None) extends CalibanClientError {
-    override def toString: String = s"Communication Error: $msg ${innerThrowable.fold("")(_.toString)}"
+    override def toString: String = s"Communication Error: $msg${innerThrowable.fold("")(t => " " + t.toString)}"
   }
 
   /**
    * An error while parsing the response from the backend
    */
   case class DecodingError(msg: String, innerThrowable: Option[Throwable] = None) extends CalibanClientError {
-    override def toString: String = s"Decoding Error: $msg ${innerThrowable.fold("")(_.toString)}"
+    override def toString: String = s"Decoding Error: $msg${innerThrowable.fold("")(t => " " + t.toString)}"
   }
 
   /**
    * A GraphQL error returned by the backend (e.g. failure during query parsing, validation or execution)
    */
   case class ServerError(errors: List[GraphQLResponseError]) extends CalibanClientError {
+
     override def toString: String =
       s"Server Error: ${errors
         .map(e =>
           s"${e.message} ${e.locations
             .getOrElse(Nil)
-            .map(loc => s" at line ${loc.line} and column ${loc.column}")
+            .map(loc => s"at line ${loc.line} and column ${loc.column}")
             .mkString(" ")}${e.path.fold("")(p =>
             s" at path ${p.map {
               case Left(value)  => s"/$value"
