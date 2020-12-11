9cc47e31cc88cbc79eee0aee108730723a426420
Pierre Ricadat <pierre.ricadat@devsisters.com>
2020-12-11 09:22:24 +0900

    Fix input fields in SchemaLoader.fromCaliban

--- core/src/main/scala/caliban/introspection/adt/__Type.scala
+++ core/src/main/scala/caliban/introspection/adt/__Type.scala
@@ -90,7 +90,7 @@ case class __Type(
             description,
             name.getOrElse(""),
             directives.getOrElse(Nil),
-            fields(__DeprecatedArgs(Some(true))).getOrElse(Nil).map(_.toInputValueDefinition)
+            inputFields.getOrElse(Nil).map(_.toInputValueDefinition)
           )
         )
       case _ => None
