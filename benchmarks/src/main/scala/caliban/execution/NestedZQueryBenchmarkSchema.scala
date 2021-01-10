8635bc43bc5d2b5df6281751719038334d0135aa
yulaw <mapadep@gmail.com>
2021-01-10 17:39:51 +0000

    Add benchmarks for different query execution strategies with different nested ZQuery layouts

---
+++ benchmarks/src/main/scala/caliban/execution/NestedZQueryBenchmarkSchema.scala
@@ -0,0 +1,86 @@
+package caliban.execution
+
+import caliban.schema.GenericSchema
+import zio.query.ZQuery
+
+object NestedZQueryBenchmarkSchema extends GenericSchema[Any] {
+  type Query[A] = ZQuery[Any, Throwable, A]
+
+  case class SimpleRoot(entities: Query[List[SimpleEntity]])
+  case class SimpleEntity(id: Int, nested: Query[Int])
+  case class MultifieldRoot(entities: Query[List[MultifieldEntity]])
+  case class MultifieldEntity(id: Int, nested0: Query[Int], nested1: Query[Int], nested2: Query[Int], nested3: Query[Int], nested4: Query[Int])
+  case class DeepRoot(entities: Query[List[DeepEntity]])
+  case class DeepEntity(next: Query[Option[DeepEntity]], nested: Query[Int])
+
+  val simple100Elements: SimpleRoot = generateSimple(100)
+  val simple1000Elements: SimpleRoot = generateSimple(1000)
+  val simple10000Elements: SimpleRoot = generateSimple(10000)
+
+  val simpleQuery: String = """{
+    entities {
+      id
+      nested
+    }
+  }""".stripMargin
+
+  val multifield100Elements: MultifieldRoot = generateMulti(100)
+  val multifield1000Elements: MultifieldRoot = generateMulti(1000)
+  val multifield10000Elements: MultifieldRoot = generateMulti(10000)
+
+  val multifieldQuery: String = """{
+    entities {
+      id
+      nested0
+      nested1
+      nested2
+      nested3
+      nested4
+    }
+  }""".stripMargin
+
+  val deep100Elements: DeepRoot = generateDeep(100)
+  val deep1000Elements: DeepRoot = generateDeep(1000)
+  val deep10000Elements: DeepRoot = generateDeep(10000)
+
+  val deepQuery: String = """{
+    entities {
+      nested
+      next {
+        nested
+        next {
+          nested
+          next {
+            nested
+            next {
+              nested
+            }
+          }
+        }
+      }
+    }
+  }""".stripMargin
+
+  private def generateSimple(n: Int) = {
+    val entities = (1 to n).map(i => SimpleEntity(i, ZQuery.succeed(i))).toList
+    SimpleRoot(ZQuery.succeed(entities))
+  }
+
+  private def generateMulti(n: Int) = {
+    val entities = (1 to n).map(i => MultifieldEntity(i, ZQuery.succeed(i), ZQuery.succeed(i), ZQuery.succeed(i), ZQuery.succeed(i), ZQuery.succeed(i))).toList
+    MultifieldRoot(ZQuery.succeed(entities))
+  }
+
+  private def generateDeep(n: Int) = {
+    def loop(n: Int): DeepEntity =
+      if (n == 0)
+        DeepEntity(ZQuery.none, ZQuery.succeed(n))
+      else {
+        val next = loop(n - 1)
+        DeepEntity(ZQuery.some(next), ZQuery.succeed(n))
+      }
+
+    val entities = (1 to n).map(_ => loop(5)).toList
+    DeepRoot(ZQuery.succeed(entities))
+  }
+}
