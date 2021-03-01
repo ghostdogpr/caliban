056e40fd3d0fb1545094dcbb0a74744ca26ca7d1
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2021-03-01 13:14:07 +0100

    mutation null value filter first draft

--- client/src/main/scala/caliban/client/Argument.scala
+++ client/src/main/scala/caliban/client/Argument.scala
@@ -9,17 +9,37 @@ import caliban.client.__Value.__NullValue
 case class Argument[+A](name: String, value: A)(implicit encoder: ArgEncoder[A]) {
   def toGraphQL(
     useVariables: Boolean,
-    variables: Map[String, (__Value, String)]
+    variables: Map[String, (__Value, String)],
+    filterNullValues: Boolean
   ): (String, Map[String, (__Value, String)]) =
     encoder.encode(value) match {
-      case `__NullValue` => ("", variables)
-      case v             =>
-        if (useVariables) {
-          val variableName = Argument.generateVariableName(name, v, variables)
-          (s"$name:$$$variableName", variables.updated(variableName, (v, encoder.formatTypeName)))
-        } else {
-          (s"$name:${v.toString}", variables)
-        }
+      case `__NullValue`         => ("", variables)
+      case v if filterNullValues =>
+        toGraphQL(filterAllNull(v), useVariables, variables)
+      case v                     =>
+        toGraphQL(v, useVariables, variables)
+    }
+
+  private def filterAllNull(v: __Value): __Value =
+    v match {
+      case __Value.__ListValue(values)   =>
+        __Value.__ListValue(values.filterNot(_.equals(__NullValue)))
+      case __Value.__ObjectValue(fields) =>
+        __Value.__ObjectValue(fields.filterNot(_._2.equals(__NullValue)))
+      case v                             =>
+        v
+    }
+
+  private def toGraphQL(
+    v: __Value,
+    useVariables: Boolean,
+    variables: Map[String, (__Value, String)]
+  ): (String, Map[String, (__Value, String)]) =
+    if (useVariables) {
+      val variableName = Argument.generateVariableName(name, v, variables)
+      (s"$name:$$$variableName", variables.updated(variableName, (v, encoder.formatTypeName)))
+    } else {
+      (s"$name:${v.toString}", variables)
     }
 }
 
