382fbc74b6f1390d7f177032a917e50ebcc5de26
Javier Martínez <martinezjavi6@gmail.com>
2021-04-17 06:19:34 +0200

    gqldoc macro for Scala 3 (#849)

---
+++ core/src/main/scala-3/caliban/Macros.scala
@@ -0,0 +1,21 @@
+package caliban 
+
+import scala.quoted._
+
+import caliban.parsing.Parser
+
+object Macros {
+
+  /**
+   * Verifies at compile-time that the given string is a valid GraphQL document.
+   * @param document a string representing a GraphQL document.
+   */
+  inline def gqldoc(inline document: String): String = ${ gqldocImpl('document) }
+
+  private def gqldocImpl(document: Expr[String])(using Quotes): Expr[String] = {
+    import quotes.reflect.report
+    document.value.fold(report.throwError("This macro can only be used with string literals."))(
+      Parser.check(_).fold(document)(e => report.throwError(s"GraphQL document is invalid: $e"))
+    )
+  }
+}
