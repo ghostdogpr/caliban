fd99cb15e4588f74fcdad2241d187beda887defd
Fredrik Wärnsberg <fredrik@warnsberg.se>
2021-05-07 16:11:43 +0200

    fix: Compilation errors

--- examples/src/main/scala/example/ziohttp/ExampleApp.scala
+++ examples/src/main/scala/example/ziohttp/ExampleApp.scala
@@ -1,26 +1,27 @@
 package example.zhttp
 
 import example.ExampleData._
-import example.ExampleService.ExampleService
 import example.{ ExampleApi, ExampleService }
 
 import zio._
-import zhttp._
 import zhttp.http._
-import caliban.ZHTTPAdapter
+import zhttp.service.Server
+import caliban.ZHttpAdapter
 
 object ExampleApp extends App {
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] = {
-    val routes = Http.route {
-      case "/api/graphql" => ZHttpAdapter.makeHttpService(interpreter)
-      case "/ws/graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
-    }
-
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
     (for {
       interpreter <- ExampleApi.api.interpreter
-      _           <- Server.start(8088, routes).forever
+      _           <- Server
+                       .start(
+                         8088,
+                         Http.route {
+                           case _ -> Root / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
+                           case _ -> Root / "ws" / "graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
+                         }
+                       )
+                       .forever
     } yield ())
       .provideCustomLayer(ExampleService.make(sampleCharacters))
       .exitCode
-  }
 }
