6dc046f0463eeb92e93ebbf7dfc5329ec37b53c8
Fredrik Wärnsberg <fredrik@warnsberg.se>
2021-05-10 10:25:56 +0200

    fix: Add graphiql

--- examples/src/main/scala/example/ziohttp/ExampleApp.scala
+++ examples/src/main/scala/example/ziohttp/ExampleApp.scala
@@ -4,11 +4,14 @@ import example.ExampleData._
 import example.{ ExampleApi, ExampleService }
 
 import zio._
+import zio.stream._
 import zhttp.http._
 import zhttp.service.Server
 import caliban.ZHttpAdapter
 
 object ExampleApp extends App {
+  val graphiql = Http.succeed(Response.http(content = HttpData.fromStream(ZStream.fromResource("graphiql.html"))))
+
   override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
     (for {
       interpreter <- ExampleApi.api.interpreter
@@ -17,7 +20,9 @@ object ExampleApp extends App {
                          8088,
                          Http.route {
                            case _ -> Root / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
-                           case _ -> Root / "ws" / "graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
+                           case _ -> Root / "ws" / "graphql"  =>
+                             ZHttpAdapter.makeWebSocketService(interpreter)
+                           case _ -> Root / "graphiql"        => graphiql
                          }
                        )
                        .forever
