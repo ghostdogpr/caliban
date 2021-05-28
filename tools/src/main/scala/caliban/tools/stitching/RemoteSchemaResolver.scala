93c9d5aed880a389cd6f1e0f67525252387bfaeb
Fredrik Wärnsberg <fredrik@warnsberg.se>
2021-05-28 15:53:11 +0200

    feat: Fix RemoteSchemaResolver.proxy

--- tools/src/main/scala/caliban/tools/stitching/RemoteSchemaResolver.scala
+++ tools/src/main/scala/caliban/tools/stitching/RemoteSchemaResolver.scala
@@ -3,6 +3,7 @@ package caliban.tools.stitching
 import zio._
 import zio.query._
 
+import caliban.GraphQL
 import caliban.GraphQLResponse
 import caliban.execution.Field
 import caliban.introspection.adt._
@@ -26,21 +27,47 @@ case class RemoteSchemaResolver(schema: __Schema, typeMap: Map[String, __Type]) 
   }
 
   def proxy[R](
-    resolver: RemoteResolver[R, ExecutionError, Field, ResponseValue]
-  ): RootSchemaBuilder[R] = RootSchemaBuilder(
-    query = Some(
-      Operation[R](
-        schema.queryType,
-        Step.MetadataFunctionStep((args: caliban.execution.Field) =>
-          Step.QueryStep(
-            ZQuery.fromEffect(resolver.run(args)).map(Step.PureStep)
+    resolver: RemoteResolver[R, ExecutionError, Field, ResponseValue],
+    mutationResolver: Option[RemoteResolver[R, ExecutionError, Field, ResponseValue]]
+  ): GraphQL[R] = {
+    def toOperation(
+      rootType: Option[__Type],
+      resolver: Option[RemoteResolver[R, ExecutionError, Field, ResponseValue]]
+    ): Option[Operation[R]] =
+      (rootType zip resolver).headOption
+        .map({ case (rootType, resolver) =>
+          Operation[R](
+            rootType,
+            Step.ObjectStep(
+              rootType.name.getOrElse(""),
+              rootType
+                .fields(__DeprecatedArgs(Some(true)))
+                .getOrElse(List())
+                .map { field =>
+                  (field.name ->
+                    Step.MetadataFunctionStep((args: caliban.execution.Field) =>
+                      Step.QueryStep(
+                        ZQuery.fromEffect(resolver.run(args)).map(Step.PureStep)
+                      )
+                    ))
+                }
+                .toMap
+            )
           )
-        )
-      )
-    ),
-    mutation = None,
-    subscription = None
-  )
+        })
+
+    val builder = RootSchemaBuilder(
+      query = toOperation(Option(schema.queryType), Option(resolver)),
+      mutation = toOperation(schema.mutationType, mutationResolver),
+      subscription = None
+    )
+
+    new GraphQL[R] {
+      protected val additionalDirectives: List[__Directive]            = schema.directives
+      protected val schemaBuilder: caliban.schema.RootSchemaBuilder[R] = builder
+      protected val wrappers: List[caliban.wrappers.Wrapper[R]]        = List()
+    }
+  }
 }
 
 object RemoteSchemaResolver {
