52aab9f6dcbefdfd64a4fe50c903625f788d369a
Stephen Marsh <stephen.david.marsh@gmail.com>
2021-06-16 21:49:03 -0400

    Fix more lint issues

--- core/src/test/scala-3/caliban/Scala3SpecificSpec.scala
+++ core/src/test/scala-3/caliban/Scala3SpecificSpec.scala
@@ -80,14 +80,15 @@ object Scala3SpecificSpec extends DefaultRunnableSpec {
         case object NullNullable          extends Nullable[Nothing]
         case object MissingNullable       extends Nullable[Nothing]
 
-        implicit def nullableArgBuilder[A](implicit ev: ArgBuilder[A]): ArgBuilder[Nullable[A]] = new ArgBuilder[Nullable[A]] {
-          def build(input: InputValue): Either[ExecutionError, Nullable[A]] = input match {
-            case NullValue => Right(NullNullable)
-            case _         => ev.build(input).map(SomeNullable(_))
-          }
+        implicit def nullableArgBuilder[A](implicit ev: ArgBuilder[A]): ArgBuilder[Nullable[A]] =
+          new ArgBuilder[Nullable[A]] {
+            def build(input: InputValue): Either[ExecutionError, Nullable[A]] = input match {
+              case NullValue => Right(NullNullable)
+              case _         => ev.build(input).map(SomeNullable(_))
+            }
 
-          override def buildMissing: Either[ExecutionError, Nullable[A]] = Right(MissingNullable)
-        }
+            override def buildMissing: Either[ExecutionError, Nullable[A]] = Right(MissingNullable)
+          }
 
         case class Wrapper(a: Nullable[String])
 
