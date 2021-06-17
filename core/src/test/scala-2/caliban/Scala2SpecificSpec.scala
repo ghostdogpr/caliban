91f3d68bdd129cfa9825dd622c67a12c5bd53bd7
Stephen Marsh <stephen.david.marsh@gmail.com>
2021-06-16 21:36:03 -0400

    Fix lint issues

--- core/src/test/scala-2/caliban/Scala2SpecificSpec.scala
+++ core/src/test/scala-2/caliban/Scala2SpecificSpec.scala
@@ -4,7 +4,7 @@ import caliban.CalibanError.ExecutionError
 import caliban.GraphQL._
 import caliban.InputValue.ObjectValue
 import caliban.TestUtils._
-import caliban.Value.{NullValue, StringValue}
+import caliban.Value.{ NullValue, StringValue }
 import caliban.introspection.adt.__DeprecatedArgs
 import caliban.schema.ArgBuilder
 import caliban.schema.SchemaSpec.introspect
@@ -47,13 +47,13 @@ object Scala2SpecificSpec extends DefaultRunnableSpec {
       test("Scala 2 buildMissing") {
         sealed abstract class Nullable[+T]
         case class SomeNullable[+T](t: T) extends Nullable[T]
-        case object NullNullable extends Nullable[Nothing]
-        case object MissingNullable extends Nullable[Nothing]
+        case object NullNullable          extends Nullable[Nothing]
+        case object MissingNullable       extends Nullable[Nothing]
 
         implicit def nullableArgBuilder[A](implicit ev: ArgBuilder[A]) = new ArgBuilder[Nullable[A]] {
           def build(input: InputValue): Either[ExecutionError, Nullable[A]] = input match {
             case NullValue => Right(NullNullable)
-            case _ => ev.build(input).map(SomeNullable(_))
+            case _         => ev.build(input).map(SomeNullable(_))
           }
 
           override def buildMissing: Either[ExecutionError, Nullable[A]] = Right(MissingNullable)
@@ -64,8 +64,8 @@ object Scala2SpecificSpec extends DefaultRunnableSpec {
         val deriviedAB = implicitly[ArgBuilder[Wrapper]]
 
         assert(deriviedAB.build(ObjectValue(Map())))(equalTo(Right(Wrapper(MissingNullable)))) &&
-          assert(deriviedAB.build(ObjectValue(Map("a" -> NullValue))))(equalTo(Right(Wrapper(NullNullable)))) &&
-          assert(deriviedAB.build(ObjectValue(Map("a" -> StringValue("x")))))(equalTo(Right(Wrapper(SomeNullable("x")))))
+        assert(deriviedAB.build(ObjectValue(Map("a" -> NullValue))))(equalTo(Right(Wrapper(NullNullable)))) &&
+        assert(deriviedAB.build(ObjectValue(Map("a" -> StringValue("x")))))(equalTo(Right(Wrapper(SomeNullable("x")))))
       }
     )
 }
--- core/src/test/scala-3/caliban/Scala3SpecificSpec.scala
+++ core/src/test/scala-3/caliban/Scala3SpecificSpec.scala
@@ -3,7 +3,7 @@ package caliban
 import caliban.CalibanError.ExecutionError
 import caliban.GraphQL._
 import caliban.InputValue.ObjectValue
-import caliban.Value.{NullValue, StringValue}
+import caliban.Value.{ NullValue, StringValue }
 import caliban.schema.Annotations.GQLInterface
 import caliban.schema.ArgBuilder
 import zio.test.Assertion._
@@ -77,13 +77,13 @@ object Scala3SpecificSpec extends DefaultRunnableSpec {
       test("Scala 3 buildMissing") {
         sealed abstract class Nullable[+T]
         case class SomeNullable[+T](t: T) extends Nullable[T]
-        case object NullNullable extends Nullable[Nothing]
-        case object MissingNullable extends Nullable[Nothing]
+        case object NullNullable          extends Nullable[Nothing]
+        case object MissingNullable       extends Nullable[Nothing]
 
         implicit def nullableArgBuilder[A](implicit ev: ArgBuilder[A]) = new ArgBuilder[Nullable[A]] {
           def build(input: InputValue): Either[ExecutionError, Nullable[A]] = input match {
             case NullValue => Right(NullNullable)
-            case _ => ev.build(input).map(SomeNullable(_))
+            case _         => ev.build(input).map(SomeNullable(_))
           }
 
           override def buildMissing: Either[ExecutionError, Nullable[A]] = Right(MissingNullable)
@@ -94,8 +94,8 @@ object Scala3SpecificSpec extends DefaultRunnableSpec {
         val deriviedAB = implicitly[ArgBuilder[Wrapper]]
 
         assert(deriviedAB.build(ObjectValue(Map())))(equalTo(Right(Wrapper(MissingNullable)))) &&
-          assert(deriviedAB.build(ObjectValue(Map("a" -> NullValue))))(equalTo(Right(Wrapper(NullNullable)))) &&
-          assert(deriviedAB.build(ObjectValue(Map("a" -> StringValue("x")))))(equalTo(Right(Wrapper(SomeNullable("x")))))
+        assert(deriviedAB.build(ObjectValue(Map("a" -> NullValue))))(equalTo(Right(Wrapper(NullNullable)))) &&
+        assert(deriviedAB.build(ObjectValue(Map("a" -> StringValue("x")))))(equalTo(Right(Wrapper(SomeNullable("x")))))
       }
     )
 }
