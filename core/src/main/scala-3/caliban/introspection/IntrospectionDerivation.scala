a069667e2e601f65d8d673334602286847f31768
Pierre Ricadat <ghostdogpr@gmail.com>
2021-07-10 14:39:10 +0900

    Make it compile

--- core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
+++ core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
@@ -8,6 +8,9 @@ import caliban.schema.Schema
 trait IntrospectionDerivation {
   implicit lazy val directiveSchema: Schema[Any, Directive]     =
     Schema.scalarSchema("Directive", None, d => StringValue(d.name))
+  implicit lazy val inputValueSchema: Schema[Any, __InputValue] = Schema.gen
+  implicit lazy val enumValueSchema: Schema[Any, __EnumValue]   = Schema.gen
+  implicit lazy val fieldSchema: Schema[Any, __Field]           = Schema.gen
   implicit lazy val typeSchema: Schema[Any, __Type]             = Schema.gen
   implicit lazy val __directiveSchema: Schema[Any, __Directive] = Schema.gen
   val introspectionSchema: Schema[Any, __Introspection]         = Schema.gen
