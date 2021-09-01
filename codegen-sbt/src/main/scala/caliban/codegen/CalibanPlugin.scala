5a3fb7325709aba8f144a8a654333bfb9e4175c2
jules Ivanic <jules.ivanic@gmail.com>
2021-09-01 22:32:14 +0800

    WIP

--- codegen-sbt/src/main/scala/caliban/codegen/CalibanPlugin.scala
+++ codegen-sbt/src/main/scala/caliban/codegen/CalibanPlugin.scala
@@ -1,7 +1,13 @@
 package caliban.codegen
 
-import sbt._
+import caliban.GraphQL
 import sbt.Keys._
+import sbt._
+
+import java.io.File
+import java.net.URLClassLoader
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
 
 object CalibanPlugin extends AutoPlugin {
   override def requires = plugins.JvmPlugin
@@ -29,14 +35,57 @@ object CalibanPlugin extends AutoPlugin {
         IO.delete((sourceDir ** "*").get)
         IO.createDirectory(sourceDir)
       },
-      calibanGenerator := CalibanSourceGenerator(
-        calibanSources.value,
-        sources.value,
-        sourceManaged.value,
-        streams.value.cacheDirectory,
-        calibanSettings.value.collect { case x: CalibanFileSettings => x },
-        calibanSettings.value.collect { case x: CalibanUrlSettings => x }
-      )
+      calibanGenerator := {
+        val log           = streams.value.log
+        log.error("♥️ ---- TOTO")
+        val cp: Seq[File] = (Compile / internalDependencyClasspath).value.files
+        val cp1           = (Compile / externalDependencyClasspath).value.files
+        log.error(s"(Compile / internalDependencyClasspath) size: ${cp.size}")
+        log.error(s"(Compile / externalDependencyClasspath) size: ${cp1.size}")
+        //cp.foreach(f => log.error(s"--- ${f.toString}"))
+        log.error(s"🤔️ ---- TOTO")
+
+        val Array(className, fieldName)         = "io.conduktor.api.gql.Gen#api".split("#")
+        val classLoader                         = new URLClassLoader((cp ++ cp1).map(_.toURI.toURL).toArray, ClassLoader.getSystemClassLoader)
+        val clazz: Either[Throwable, Class[_]]  =
+          try Right(classLoader.loadClass(className))
+          catch {
+            case e: Throwable => Left(e)
+          }
+        val obzect: Either[Throwable, Class[_]] =
+          try Right(classLoader.loadClass(className + "$"))
+          catch {
+            case e: Throwable => Left(e)
+          }
+
+        log.error(s"😭 --- $clazz")
+        log.error(s"😭😭 --- $obzect")
+
+        val method   = clazz.toOption.get.getDeclaredMethod(fieldName)
+        val instance = obzect.toOption.get.getDeclaredField("MODULE$")
+        val api      = method.invoke(instance).asInstanceOf[GraphQL[_]]
+        Files.write(
+          new File("/Users/jules/conduktor/workspace/scala-gql-template/modules/api/src/main/graphql").toPath,
+          api.render.getBytes(StandardCharsets.UTF_8)
+        )
+
+        val res = CalibanSourceGenerator(
+          calibanSources.value,
+          sources.value,
+          sourceManaged.value,
+          streams.value.cacheDirectory,
+          calibanSettings.value.collect { case x: CalibanFileSettings => x },
+          calibanSettings.value.collect { case x: CalibanUrlSettings => x }
+        )
+
+        log.error(s"♥️ ---- ♥️")
+        res.foreach { file =>
+          log.error(s"--- $file")
+        }
+        log.error(s"🤔️ ---- 🤔️️")
+
+        res
+      }
     )
   )
 
--- tools/src/main/scala/caliban/tools/Options.scala
+++ tools/src/main/scala/caliban/tools/Options.scala
@@ -4,6 +4,11 @@ import caliban.GraphQL
 import zio.config.magnolia.DeriveConfigDescriptor.descriptor
 import zio.config.{ read, ConfigDescriptor, ConfigSource }
 
+import java.io.File
+import java.lang.reflect.{ Constructor, Field }
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+
 sealed trait GenSource
 object GenSource {
   case object Url                              extends GenSource
@@ -96,9 +101,64 @@ object Options {
     }
   }
 
+  def fromCalibanCode(codePath: String, toPath: String, other: List[String]): Option[Options] = {
+    def loadGraphQL: GraphQL[_] = {
+      val Array(className, methodName) = codePath.split("#")
+      val clazz: Class[_]              = this.getClass.getClassLoader.loadClass(className)
+      val constructor: Constructor[_]  = clazz.getDeclaredConstructors.head
+      val method: Field                = clazz.getDeclaredField(methodName)
+      val api                          = method.get(constructor.newInstance()).asInstanceOf[GraphQL[_]]
+      Files.write(new File(toPath).toPath, api.render.getBytes(StandardCharsets.UTF_8))
+      api
+    }
+
+    val configSource: ConfigSource =
+      ConfigSource.fromCommandLineArgs(
+        args = other,
+        keyDelimiter = Some('.'),
+        valueDelimiter = Some(',')
+      )
+
+    val configDescriptor: ConfigDescriptor[RawOptions] = descriptor[RawOptions] from configSource
+
+    read(configDescriptor).toOption.map { rawOpts =>
+      Options(
+        GenSource.Caliban(loadGraphQL),
+        codePath,
+        toPath,
+        rawOpts.scalafmtPath,
+        rawOpts.headers.map {
+          _.flatMap { rawHeader =>
+            rawHeader.split(":").toList match {
+              case name :: values if values.nonEmpty => Some(Header(name, values.mkString(":")))
+              case _                                 => None
+            }
+          }
+        },
+        rawOpts.packageName,
+        rawOpts.clientName,
+        rawOpts.genView,
+        rawOpts.effect,
+        rawOpts.scalarMappings.map {
+          _.flatMap { rawMapping =>
+            rawMapping.split(":").toList match {
+              case name :: value :: Nil => Some(name -> value)
+              case _                    => None
+            }
+          }.toMap
+        },
+        rawOpts.imports,
+        rawOpts.abstractEffectType,
+        rawOpts.splitFiles,
+        rawOpts.enableFmt,
+        rawOpts.extensibleEnums
+      )
+    }
+  }
+
   def fromArgs(args: List[String]): Option[Options] =
     args match {
-      case "caliban" :: codePath :: toPath :: other => ??? // TODO Jules
+      case "caliban" :: codePath :: toPath :: other => fromCalibanCode(codePath, toPath, other)
       case "url" :: schemaPath :: toPath :: other   => fromUrlOrFile(GenSource.Url, schemaPath, toPath, other)
       case "file" :: schemaPath :: toPath :: other  => fromUrlOrFile(GenSource.File, schemaPath, toPath, other)
       case _                                        => None
