0802f7a3114d818636fd9499e4f3eddad605f3e1
jules Ivanic <jules.ivanic@gmail.com>
2021-09-08 14:35:43 +0800

    Review: Simplify code

--- tools/src/main/scala/caliban/tools/compiletime/CompileTime.scala
+++ tools/src/main/scala/caliban/tools/compiletime/CompileTime.scala
@@ -3,27 +3,22 @@ package caliban.tools.compiletime
 import caliban.GraphQL
 import caliban.tools.Codegen.GenType
 import caliban.tools.{ Codegen, SchemaLoader }
-import zio.{ ExitCode, Task, URIO, ZEnv }
+import zio.{ ExitCode, URIO, ZEnv }
 
 object CompileTime {
 
   def generateClient[R](args: List[String], api: GraphQL[R]): URIO[ZEnv, ExitCode] =
     args match {
       case toPath :: calibanSettings =>
-        (
-          for {
-            _      <- Task.unit
-            options = CompileTimeUtils.calibanCommonSettingsEquivalence
-                        .from(calibanSettings)
-                        .toOptions(schemaPath = "", toPath = toPath)
-            _      <- Codegen
-                        .generate(
-                          SchemaLoader.fromCaliban(api),
-                          options,
-                          GenType.Client
-                        )
-          } yield ()
-        ).exitCode
+        Codegen
+          .generate(
+            SchemaLoader.fromCaliban(api),
+            CompileTimeUtils.calibanCommonSettingsEquivalence
+              .from(calibanSettings)
+              .toOptions(schemaPath = "", toPath = toPath),
+            GenType.Client
+          )
+          .exitCode
       case _                         => URIO.unit.exitCode
     }
 
