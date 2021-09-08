bd320e81dd2cb69220ed5740cdb2b7562807dd20
jules Ivanic <jules.ivanic@gmail.com>
2021-09-08 14:49:33 +0800

    Review: Simplify test project

--- codegen-sbt/src/sbt-test/compiletime-codegen/client-and-package/modules/server/src/main/scala/io/guizmaii/poc/caliban/server/PostService.scala
+++ codegen-sbt/src/sbt-test/compiletime-codegen/client-and-package/modules/server/src/main/scala/io/guizmaii/poc/caliban/server/PostService.scala
@@ -1,16 +1,12 @@
 package io.guizmaii.poc.caliban.server
 
 import zio._
-import zio.prelude.Equal
 import zio.stream.ZStream
 
 import java.util.UUID
 
 final case class PostId(id: UUID)
 final case class AuthorName(name: String)
-object AuthorName {
-  implicit val eq: Equal[AuthorName] = Equal.default
-}
 final case class PostTitle(title: String)
 final case class PostContent(content: String)
 
