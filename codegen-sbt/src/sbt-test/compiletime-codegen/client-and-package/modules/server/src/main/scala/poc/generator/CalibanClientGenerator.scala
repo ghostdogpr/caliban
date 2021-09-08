f5a69bcec43cec4f2e2445baff4f60d3ae52caf3
jules Ivanic <jules.ivanic@gmail.com>
2021-09-08 16:40:53 +0800

    Fix tests

--- codegen-sbt/src/sbt-test/compiletime-codegen/client-and-package/modules/server/src/main/scala/poc/generator/CalibanClientGenerator.scala
+++ codegen-sbt/src/sbt-test/compiletime-codegen/client-and-package/modules/server/src/main/scala/poc/generator/CalibanClientGenerator.scala
@@ -1,10 +1,18 @@
 package poc.generator
 
 import caliban.tools.compiletime.CompileTime
+import caliban.tools.compiletime.CompileTime.GenerateClientsSettings
 import io.guizmaii.poc.caliban.server.GraphQLApi
 import zio.{ExitCode, URIO}
 
 private[generator] object CalibanClientGenerator extends zio.App {
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    CompileTime.generateClient(args, GraphQLApi.api)
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    val settings =
+      GenerateClientsSettings(
+        clientName = "CalibanClient",
+        packageName = "io.guizmaii.poc.caliban.client.generated"
+      )
+
+    CompileTime.generateClient(args)(settings, GraphQLApi.api)
+  }
 }
