dc16538d873adf52c59fa450f9a1b2ed6d710658
jules Ivanic <jules.ivanic@gmail.com>
2021-09-13 18:31:08 +0800

    Try( Fix errors:
    ```scala
    [error] Caused by: coursierapi.error.DownloadingArtifactsError: https://repo1.maven.org/maven2/org/scala-lang/modules/scala-parallel-collections_2.13/1.0.3/scala-parallel-collections_2.13-1.0.3.jar: checksum not found: /home/circleci/.cache/coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/modules/scala-parallel-collections_2.13/1.0.3/scala-parallel-collections_2.13-1.0.3.jar
    ```
    
    )

--- tools/src/main/scala/caliban/tools/Formatter.scala
+++ tools/src/main/scala/caliban/tools/Formatter.scala
@@ -1,28 +1,38 @@
 package caliban.tools
 
-import java.nio.file.{ Files, Paths }
-
 import org.scalafmt.interfaces.Scalafmt
-import zio.Task
+import zio.RIO
+import zio.blocking.{ effectBlocking, Blocking }
+import zio.prelude._
+
+import java.nio.file.{ Files, Path, Paths }
 
 object Formatter {
 
-  def format(str: String, fmtPath: Option[String]): Task[String]                                  =
+  def format(str: String, fmtPath: Option[String]): RIO[Blocking, String] =
     format(List("Nil.scala" -> str), fmtPath).map(_.head._2)
 
-  def format(strs: List[(String, String)], fmtPath: Option[String]): Task[List[(String, String)]] = Task {
-    val scalafmt          = Scalafmt.create(this.getClass.getClassLoader)
-    val defaultConfigPath = Paths.get(".scalafmt.conf")
-    val defaultConfig     =
-      if (Files.exists(defaultConfigPath)) defaultConfigPath else Paths.get("")
-    val config            = fmtPath.fold(defaultConfig)(Paths.get(_))
-    val result            = strs.map { case (name, code) =>
-      name -> scalafmt
-        .withRespectVersion(false)
-        .format(config, Paths.get(s"$name.scala"), code)
-    }
-    scalafmt.clear()
-    result
+  def format(strs: List[(String, String)], fmtPath: Option[String]): RIO[Blocking, List[(String, String)]] = {
+    def configuration: RIO[Blocking, Path] =
+      effectBlocking {
+        @inline def defaultConfigPath = Paths.get(".scalafmt.conf")
+        @inline def defaultConfig     = if (Files.exists(defaultConfigPath)) defaultConfigPath else Paths.get("")
+
+        fmtPath.fold(defaultConfig)(Paths.get(_))
+      }
+
+    for {
+      scalafmt <- effectBlocking(Scalafmt.create(this.getClass.getClassLoader))
+      config   <- configuration
+      result   <- strs.forEach { case (name, code) =>
+                    effectBlocking {
+                      name -> scalafmt
+                        .withRespectVersion(false)
+                        .format(config, Paths.get(s"$name.scala"), code)
+                    }
+                  }
+      _        <- effectBlocking(scalafmt.clear())
+    } yield result
   }
 
 }
