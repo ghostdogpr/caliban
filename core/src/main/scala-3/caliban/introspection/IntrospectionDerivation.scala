f3311689fc463cac47e9d79cd741a2946637b6a6
Pierre Ricadat <pierre.ricadat@devsisters.com>
2021-10-20 11:33:40 +0900

    Remove weird trick

--- core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
+++ core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
@@ -1,17 +1,18 @@
 package caliban.introspection
 
+import caliban.InputValue
 import caliban.Value.StringValue
 import caliban.introspection.adt._
 import caliban.parsing.adt.Directive
 import caliban.schema.Schema
 
 trait IntrospectionDerivation {
-  implicit lazy val directiveSchema: Schema[Any, Directive]     =
-    Schema.scalarSchema("Directive", None, d => StringValue(d.name))
-  implicit lazy val inputValueSchema: Schema[Any, __InputValue] = Schema.gen
-  implicit lazy val enumValueSchema: Schema[Any, __EnumValue]   = Schema.gen
-  implicit lazy val fieldSchema: Schema[Any, __Field]           = Schema.gen
-  implicit lazy val typeSchema: Schema[Any, __Type]             = Schema.gen
-  implicit lazy val __directiveSchema: Schema[Any, __Directive] = Schema.gen
-  val introspectionSchema: Schema[Any, __Introspection]         = Schema.gen
+  implicit lazy val inputValue: Schema[Any, InputValue]           = Schema.gen
+  implicit lazy val directiveSchema: Schema[Any, Directive]       = Schema.gen
+  implicit lazy val __inputValueSchema: Schema[Any, __InputValue] = Schema.gen
+  implicit lazy val enumValueSchema: Schema[Any, __EnumValue]     = Schema.gen
+  implicit lazy val fieldSchema: Schema[Any, __Field]             = Schema.gen
+  implicit lazy val typeSchema: Schema[Any, __Type]               = Schema.gen
+  implicit lazy val __directiveSchema: Schema[Any, __Directive]   = Schema.gen
+  val introspectionSchema: Schema[Any, __Introspection]           = Schema.gen
 }
