54d454b71fa2c7fa4399da83182e2fb6276f8d8c
Pierre Ricadat <pierre.ricadat@devsisters.com>
2021-10-21 12:12:36 +0900

    Simplify

--- core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
+++ core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
@@ -7,11 +7,9 @@ import caliban.parsing.adt.Directive
 import caliban.schema.Schema
 
 trait IntrospectionDerivation {
+  import Schema._
+
   implicit lazy val inputValueSchema: Schema[Any, InputValue]     = Schema.gen
-  implicit lazy val directiveSchema: Schema[Any, Directive]       = Schema.gen
-  implicit lazy val __inputValueSchema: Schema[Any, __InputValue] = Schema.gen
-  implicit lazy val enumValueSchema: Schema[Any, __EnumValue]     = Schema.gen
-  implicit lazy val fieldSchema: Schema[Any, __Field]             = Schema.gen
   implicit lazy val typeSchema: Schema[Any, __Type]               = Schema.gen
   implicit lazy val __directiveSchema: Schema[Any, __Directive]   = Schema.gen
   val introspectionSchema: Schema[Any, __Introspection]           = Schema.gen
