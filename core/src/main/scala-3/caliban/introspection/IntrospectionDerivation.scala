fea6b553f60216614f710abbc56eb905580154e9
Pierre Ricadat <pierre.ricadat@devsisters.com>
2021-10-21 12:16:51 +0900

    fmt

--- core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
+++ core/src/main/scala-3/caliban/introspection/IntrospectionDerivation.scala
@@ -9,8 +9,8 @@ import caliban.schema.Schema
 trait IntrospectionDerivation {
   import Schema._
 
-  implicit lazy val inputValueSchema: Schema[Any, InputValue]     = Schema.gen
-  implicit lazy val typeSchema: Schema[Any, __Type]               = Schema.gen
-  implicit lazy val __directiveSchema: Schema[Any, __Directive]   = Schema.gen
-  val introspectionSchema: Schema[Any, __Introspection]           = Schema.gen
+  implicit lazy val inputValueSchema: Schema[Any, InputValue]   = Schema.gen
+  implicit lazy val typeSchema: Schema[Any, __Type]             = Schema.gen
+  implicit lazy val __directiveSchema: Schema[Any, __Directive] = Schema.gen
+  val introspectionSchema: Schema[Any, __Introspection]         = Schema.gen
 }
