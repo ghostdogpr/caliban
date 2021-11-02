2462d46adb32889316b58686752b01b6b9999541
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2021-11-02 19:22:54 +0900

    Upgrade Scala to 2.13.7 (#1122)

--- core/src/main/scala-2/caliban/parsing/parsers/NumberParsers.scala
+++ core/src/main/scala-2/caliban/parsing/parsers/NumberParsers.scala
@@ -4,17 +4,19 @@ import caliban.Value._
 import fastparse._
 
 private[caliban] trait NumberParsers extends StringParsers {
-  def negativeSign[_: P]: P[Unit] = P("-")
-  def nonZeroDigit[_: P]: P[Unit] = P(CharIn("1-9"))
-  def digit[_: P]: P[Unit]        = P("0" | nonZeroDigit)
-  def integerPart[_: P]: P[Unit]  = P((negativeSign.? ~~ "0") | (negativeSign.? ~~ nonZeroDigit ~~ digit.repX))
-  def intValue[_: P]: P[IntValue] = integerPart.!.map(IntValue(_))
+  def negativeSign(implicit ev: P[Any]): P[Unit] = P("-")
+  def nonZeroDigit(implicit ev: P[Any]): P[Unit] = P(CharIn("1-9"))
+  def digit(implicit ev: P[Any]): P[Unit]        = P("0" | nonZeroDigit)
+  def integerPart(implicit ev: P[Any]): P[Unit]  = P(
+    (negativeSign.? ~~ "0") | (negativeSign.? ~~ nonZeroDigit ~~ digit.repX)
+  )
+  def intValue(implicit ev: P[Any]): P[IntValue] = integerPart.!.map(IntValue(_))
 
-  def sign[_: P]: P[Unit]              = P("-" | "+")
-  def exponentIndicator[_: P]: P[Unit] = P(CharIn("eE"))
-  def exponentPart[_: P]: P[Unit]      = P(exponentIndicator ~~ sign.? ~~ digit.repX(1))
-  def fractionalPart[_: P]: P[Unit]    = P("." ~~ digit.repX(1))
-  def floatValue[_: P]: P[FloatValue]  =
+  def sign(implicit ev: P[Any]): P[Unit]              = P("-" | "+")
+  def exponentIndicator(implicit ev: P[Any]): P[Unit] = P(CharIn("eE"))
+  def exponentPart(implicit ev: P[Any]): P[Unit]      = P(exponentIndicator ~~ sign.? ~~ digit.repX(1))
+  def fractionalPart(implicit ev: P[Any]): P[Unit]    = P("." ~~ digit.repX(1))
+  def floatValue(implicit ev: P[Any]): P[FloatValue]  =
     P(
       (integerPart ~~ fractionalPart) | (integerPart ~~ exponentPart) | (integerPart ~~ fractionalPart ~~ exponentPart)
     ).!.map(FloatValue(_))
