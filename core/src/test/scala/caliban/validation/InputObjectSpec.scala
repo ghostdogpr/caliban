880ae2ffacdc7bcdedcbeb37f6cf17318f6ed59b
Pierre Ricadat <ghostdogpr@gmail.com>
2021-11-17 18:32:44 +0900

    Coerce int values into float when expected

--- core/src/test/scala/caliban/validation/InputObjectSpec.scala
+++ core/src/test/scala/caliban/validation/InputObjectSpec.scala
@@ -24,7 +24,7 @@ object InputObjectSpec extends DefaultRunnableSpec {
           int <- gql.interpreter
           res <- int.execute(query)
         } yield assert(res.errors.headOption)(
-          isSome((isSubtype[CalibanError.ValidationError](anything)))
+          isSome(isSubtype[CalibanError.ValidationError](anything))
         )
       },
       testM("fails if a non-null field on an input object is null from variables") {
@@ -53,7 +53,7 @@ object InputObjectSpec extends DefaultRunnableSpec {
                    )
                  )
         } yield assert(res.errors.headOption)(
-          isSome((isSubtype[CalibanError.ValidationError](anything)))
+          isSome(isSubtype[CalibanError.ValidationError](anything))
         )
       },
       testM("allow null passed to optional enum") {
@@ -133,6 +133,34 @@ object InputObjectSpec extends DefaultRunnableSpec {
                        Map(
                          "input" -> InputValue.ObjectValue(
                            Map("list" -> Value.NullValue)
+                         )
+                       )
+                     )
+                   )
+                 )
+        } yield assert(res.errors)(isEmpty)
+      },
+      testM("allow Int passed to Float field") {
+        val query =
+          """query QueryName($input: TestInputObjectInput!) {
+            |  query(input: $input)
+            |}""".stripMargin
+
+        case class TestInputObject(float: Float)
+        case class TestInput(input: TestInputObject)
+        case class TestOutput(value: String)
+        case class Query(query: TestInput => String)
+        val gql = graphQL(RootResolver(Query(_.input.float.toString)))
+
+        for {
+          int <- gql.interpreter
+          res <- int.executeRequest(
+                   GraphQLRequest(
+                     query = Some(query),
+                     variables = Some(
+                       Map(
+                         "input" -> InputValue.ObjectValue(
+                           Map("float" -> Value.IntValue(42))
                          )
                        )
                      )
