509ea4909839d73fd050976241a6040041fef789
Alix BAGGIO <AlixBa@users.noreply.github.com>
2021-11-25 12:37:28 +0100

    chore: update scalafmt (#1174)

--- tools/src/main/scala/caliban/tools/Formatter.scala
+++ tools/src/main/scala/caliban/tools/Formatter.scala
@@ -5,7 +5,8 @@ import org.scalafmt.interfaces.Scalafmt
 import zio.RIO
 import zio.blocking.{ effectBlocking, Blocking }
 
-import java.nio.file.{ Files, Path, Paths }
+import java.nio.file.{ Files, Path, Paths, StandardCopyOption }
+import java.util.jar.JarFile
 
 object Formatter {
 
@@ -16,7 +17,23 @@ object Formatter {
     effectBlocking {
       val config: Path = {
         @inline def defaultConfigPath = Paths.get(".scalafmt.conf")
-        @inline def defaultConfig     = if (Files.exists(defaultConfigPath)) defaultConfigPath else Paths.get("")
+        @inline def defaultConfig     =
+          if (Files.exists(defaultConfigPath)) defaultConfigPath
+          else {
+            val defaultScalafmtCalibanToolsFile = "default.scalafmt.conf"
+            val uri                             = this.getClass.getClassLoader.getResource(defaultScalafmtCalibanToolsFile).toURI
+            uri.getScheme match {
+              case "file" => Paths.get(uri)
+              case "jar"  =>
+                // scalafmt can't access a file inside a JAR so we'll copy the content into a temp file
+                val jar            = new JarFile(this.getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath)
+                val file           = Files.createTempFile(null, null)
+                val scalafmtConfig = jar.getInputStream(jar.getEntry(defaultScalafmtCalibanToolsFile))
+                Files.copy(scalafmtConfig, file, StandardCopyOption.REPLACE_EXISTING)
+                file
+              case _      => Paths.get("")
+            }
+          }
 
         fmtPath.fold(defaultConfig)(Paths.get(_))
       }
@@ -24,8 +41,6 @@ object Formatter {
       val scalafmt =
         Scalafmt
           .create(this.getClass.getClassLoader)
-          .withRespectVersion(false)
-          .withDefaultVersion("2.7.5")                           // For retro-compatibility
           .withReporter(new ConsoleScalafmtReporter(System.out)) // defaults prints everything to System.err
 
       val result = strs.map { case (name, code) => name -> scalafmt.format(config, Paths.get(s"$name.scala"), code) }
