388e067e77c6a934d24953f8f2b94246460e2bd5
pascal <pascal.meheut@gmail.com>
2022-01-09 13:42:54 +0100

    Now using scalafmt-interfaces, no more direct dependencies

--- tools/src/main/scala/caliban/tools/Formatter.scala
+++ tools/src/main/scala/caliban/tools/Formatter.scala
@@ -1,5 +1,7 @@
 package caliban.tools
 
+import org.scalafmt.interfaces.ScalafmtReporter
+import org.scalafmt.interfaces.Scalafmt
 import zio.RIO
 import zio.blocking.{ effectBlocking, Blocking }
 
@@ -13,7 +15,51 @@ object Formatter {
 
   def format(strs: List[(String, String)], fmtPath: Option[String]): RIO[Blocking, List[(String, String)]] =
     effectBlocking {
-      strs
+      val config: Path = {
+        @inline def defaultConfigPath = Paths.get(".scalafmt.conf")
+        @inline def defaultConfig     =
+          if (Files.exists(defaultConfigPath)) defaultConfigPath
+          else {
+            val defaultScalafmtCalibanToolsFile = "default.scalafmt.conf"
+            val uri                             = this.getClass.getClassLoader.getResource(defaultScalafmtCalibanToolsFile).toURI
+            uri.getScheme match {
+              case "file" => Paths.get(uri)
+              case "jar"  =>
+                // scalafmt can't access a file inside a JAR so we'll copy the content into a temp file
+                val jar            = new JarFile(this.getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath)
+                val file           = Files.createTempFile(null, null)
+                val scalafmtConfig = jar.getInputStream(jar.getEntry(defaultScalafmtCalibanToolsFile))
+                Files.copy(scalafmtConfig, file, StandardCopyOption.REPLACE_EXISTING)
+                file
+              case _      => Paths.get("")
+            }
+          }
+
+        fmtPath.fold(defaultConfig)(Paths.get(_))
+      }
+
+      val scalafmt = buildScalaFmt()
+
+      val result = strs.map { case (name, code) => name -> scalafmt.format(config, Paths.get(s"$name.scala"), code) }
+      scalafmt.clear()
+      result
     }.retryN(3) // We have to retry because of the bug detailed here: https://github.com/scalameta/scalafmt/issues/2793
 
+  def buildScalaFmt(): Scalafmt = {
+    import coursierapi.{Dependency, Fetch, Module}
+    import org.scalafmt.interfaces.{Scalafmt, ScalafmtClassLoader, ScalafmtReporter}
+
+    import java.io.{File, PrintStream}
+    import java.net.URLClassLoader
+    import scala.jdk.CollectionConverters._
+    val files = Fetch.create()
+      .addDependencies(Dependency.of("org.scalameta", "scalafmt-dynamic_2.13", "3.1.2"))
+      .fetch()
+    val classLoader = new URLClassLoader(files.asScala.toArray.map(_.toURI().toURL()), this.getClass.getClassLoader)
+    val fmt = Scalafmt.create(classLoader)
+    val reporterClass = classLoader.loadClass("org.scalafmt.dynamic.ConsoleScalafmtReporter")
+    val constructor = reporterClass.getConstructor(classOf[PrintStream]);
+    val reporter = constructor.newInstance(System.err).asInstanceOf[ScalafmtReporter]
+    fmt.withReporter(reporter)
+  }
 }
