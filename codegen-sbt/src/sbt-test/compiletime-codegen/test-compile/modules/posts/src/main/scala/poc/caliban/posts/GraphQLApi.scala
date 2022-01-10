6753cd3d68099b20794cbd24828ba7c731f66aa1
Jules Ivanic <guizmaii@users.noreply.github.com>
2022-01-10 12:02:50 +0800

    Fix compilation of projects using the ConpileTimeCaliban sbt plugin and the scalac `-Werror` flag (#1247)

--- codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/modules/posts/src/main/scala/poc/caliban/posts/GraphQLApi.scala
+++ codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/modules/posts/src/main/scala/poc/caliban/posts/GraphQLApi.scala
@@ -8,6 +8,8 @@ import zio._
 import zio.duration.durationInt
 import zio.stream.ZStream
 
+import scala.language.higherKinds
+
 object Operations {
 
   final case class CreatePostMutationParams(authorName: AuthorName, title: PostTitle, content: PostContent)
