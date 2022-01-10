7da7c346f4beea025cd68e4afc674a59a8988c3c
pascal <pascal.meheut@gmail.com>
2022-01-10 08:35:21 +0100

    Fixed foromatting

--- tools/src/main/scala/caliban/tools/Formatter.scala
+++ tools/src/main/scala/caliban/tools/Formatter.scala
@@ -46,27 +46,28 @@ object Formatter {
     }.retryN(3) // We have to retry because of the bug detailed here: https://github.com/scalameta/scalafmt/issues/2793
 
   def buildScalaFmt(): Scalafmt = {
-    import coursierapi.{Dependency, Fetch, Module}
-    import org.scalafmt.interfaces.{Scalafmt, ScalafmtClassLoader, ScalafmtReporter}
+    import coursierapi.{ Dependency, Fetch, Module }
+    import org.scalafmt.interfaces.{ Scalafmt, ScalafmtClassLoader, ScalafmtReporter }
 
-    import java.io.{File, PrintStream}
+    import java.io.{ File, PrintStream }
     import java.net.URLClassLoader
     import scala.jdk.CollectionConverters._
 
     val scalaVersion = BuildInfo.scalaPartialVersion match {
       case Some((2, 12)) => "2.12"
       case Some((2, 13)) => "2.13"
-      case Some((3, _)) => "2.13"
-      case _ => "2.12"
+      case Some((3, _))  => "2.13"
+      case _             => "2.12"
     }
 
-    val files = Fetch.create()
+    val files                      = Fetch
+      .create()
       .addDependencies(Dependency.of("org.scalameta", s"scalafmt-dynamic_$scalaVersion", BuildInfo.scalafmtVersion))
       .fetch()
-    val classLoader = new URLClassLoader(files.asScala.toArray.map(_.toURI().toURL()), this.getClass.getClassLoader)
-    val fmt = Scalafmt.create(classLoader)
-    val reporterClass = classLoader.loadClass("org.scalafmt.dynamic.ConsoleScalafmtReporter")
-    val constructor = reporterClass.getConstructor(classOf[PrintStream]);
+    val classLoader                = new URLClassLoader(files.asScala.toArray.map(_.toURI().toURL()), this.getClass.getClassLoader)
+    val fmt                        = Scalafmt.create(classLoader)
+    val reporterClass              = classLoader.loadClass("org.scalafmt.dynamic.ConsoleScalafmtReporter")
+    val constructor                = reporterClass.getConstructor(classOf[PrintStream]);
     val reporter: ScalafmtReporter = constructor.newInstance(System.err).asInstanceOf[ScalafmtReporter]
     fmt.withReporter(reporter)
   }
