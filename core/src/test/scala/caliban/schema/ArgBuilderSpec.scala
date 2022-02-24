b6bdb21859f4b90deabff9d0b3374757564578ff
Mathieu Dulac <mat.dulac@gmail.com>
2022-02-24 17:04:26 +0100

    Handle Long as String

--- core/src/test/scala/caliban/schema/ArgBuilderSpec.scala
+++ core/src/test/scala/caliban/schema/ArgBuilderSpec.scala
@@ -24,6 +24,15 @@ object ArgBuilderSpec extends DefaultRunnableSpec {
         )
       )
     ),
+    suite("long")(
+      testM("Long from string")(
+        check(Gen.anyLong) { value =>
+          assert(ArgBuilder.long.build(StringValue(s"$value")))(
+            isRight(equalTo(value))
+          )
+        }
+      )
+    ),
     suite("java.time")(
       test("Instant from epoch")(
         assert(ArgBuilder.instantEpoch.build(IntValue.LongNumber(100)))(
