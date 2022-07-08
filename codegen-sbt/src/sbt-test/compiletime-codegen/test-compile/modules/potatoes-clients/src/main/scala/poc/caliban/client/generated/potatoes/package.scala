74582a8cd758abd524acac35a0882173ca53b31d
pascal <pascal.meheut@gmail.com>
2022-07-08 19:09:22 +0000

    Working again on codegen for scala3 and its tests

---
+++ codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/modules/potatoes-clients/src/main/scala/poc/caliban/client/generated/potatoes/package.scala
@@ -0,0 +1,10 @@
+package poc.caliban.client.generated
+
+package object potatoes {
+  type Potato
+  type Name
+  type Query = _root_.caliban.client.Operations.RootQuery
+  type Mutation = _root_.caliban.client.Operations.RootMutation
+  type Subscription = _root_.caliban.client.Operations.RootSubscription
+}
+
