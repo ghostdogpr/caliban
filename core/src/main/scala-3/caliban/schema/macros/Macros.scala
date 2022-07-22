6005ab2bd4a07f7075951b8846fd85dfb5d892fa
Pierre Ricadat <pierre.ricadat@devsisters.com>
2022-07-22 16:05:11 +0900

    Fix the other macro

--- core/src/main/scala-3/caliban/schema/macros/Macros.scala
+++ core/src/main/scala-3/caliban/schema/macros/Macros.scala
@@ -26,8 +26,7 @@ private[caliban] object Macros {
     Expr.ofList {
       tpe.typeSymbol.primaryConstructor.paramSymss.flatten.map { field =>
         Expr(field.name) -> field.annotations.filter { a =>
-          a.tpe.typeSymbol.maybeOwner.isNoSymbol ||
-          a.tpe.typeSymbol.owner.fullName != "scala.annotation.internal"
+          a.tpe.typeSymbol.maybeOwner.isNoSymbol || a.tpe.typeSymbol.owner.fullName.startsWith("caliban.schema.Annotations")
         }.map(_.asExpr.asInstanceOf[Expr[Any]])
       }.filter(_._2.nonEmpty).map((name, anns) => Expr.ofTuple(name, Expr.ofList(anns)))
     }
