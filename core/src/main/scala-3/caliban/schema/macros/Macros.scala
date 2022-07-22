bbb8025390aa3f4de90f59c005a399087f7517e6
Pierre Ricadat <pierre.ricadat@devsisters.com>
2022-07-22 16:05:52 +0900

    fmt

--- core/src/main/scala-3/caliban/schema/macros/Macros.scala
+++ core/src/main/scala-3/caliban/schema/macros/Macros.scala
@@ -15,7 +15,8 @@ private[caliban] object Macros {
     val tpe = TypeRepr.of[T]
     Expr.ofList {
       tpe.typeSymbol.annotations.filter { a =>
-        a.tpe.typeSymbol.maybeOwner.isNoSymbol || a.tpe.typeSymbol.owner.fullName.startsWith("caliban.schema.Annotations")
+        a.tpe.typeSymbol.maybeOwner.isNoSymbol ||
+        a.tpe.typeSymbol.owner.fullName.startsWith("caliban.schema.Annotations")
       }.map(_.asExpr.asInstanceOf[Expr[Any]])
     }
   }
@@ -26,7 +27,8 @@ private[caliban] object Macros {
     Expr.ofList {
       tpe.typeSymbol.primaryConstructor.paramSymss.flatten.map { field =>
         Expr(field.name) -> field.annotations.filter { a =>
-          a.tpe.typeSymbol.maybeOwner.isNoSymbol || a.tpe.typeSymbol.owner.fullName.startsWith("caliban.schema.Annotations")
+          a.tpe.typeSymbol.maybeOwner.isNoSymbol ||
+          a.tpe.typeSymbol.owner.fullName.startsWith("caliban.schema.Annotations")
         }.map(_.asExpr.asInstanceOf[Expr[Any]])
       }.filter(_._2.nonEmpty).map((name, anns) => Expr.ofTuple(name, Expr.ofList(anns)))
     }
