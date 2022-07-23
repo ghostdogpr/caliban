4b548b33aa0a5c122f073305c1683b80d27c53ab
pascal <pascal.meheut@gmail.com>
2022-07-23 11:47:10 +0200

    Merged scala-2.12 & 2.13 into scala-2, removed generated files from git added by mistake

--- tools/src/main/scala-2.12/caliban/tools/DescriptorUtils.scala
+++
@@ -1,9 +0,0 @@
-package caliban.tools
-
-import zio.config.magnolia.Descriptor
-import zio.config.{ ConfigDescriptor, ConfigSource }
-
-object DescriptorUtils {
-  def from[A](configSource: ConfigSource)(implicit config: Descriptor[A]): ConfigDescriptor[A] =
-    Descriptor.descriptor[A] from configSource
-}
