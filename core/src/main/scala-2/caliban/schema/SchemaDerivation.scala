8d962f6a24a00dcfad65399569fe05e00c3b4b57
jules Ivanic <jules.ivanic@gmail.com>
2022-10-14 07:26:35 +0200

    Update Scala 2.12 to `2.12.17`
    Fixes issues with scala-xml bincompat issues
    See https://github.com/scala/bug/issues/12632

--- core/src/main/scala-2/caliban/schema/SchemaDerivation.scala
+++ core/src/main/scala-2/caliban/schema/SchemaDerivation.scala
@@ -4,7 +4,7 @@ import caliban.Value._
 import caliban.introspection.adt._
 import caliban.parsing.adt.Directive
 import caliban.schema.Annotations._
-import caliban.schema.Step._
+import caliban.schema.Step.{ PureStep => _, _ }
 import caliban.schema.Types._
 import magnolia._
 
