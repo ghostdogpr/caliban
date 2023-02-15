7b410a1b180c119c06eec2dde31bc082740fcb8e
Johan Brodin <johan.brodin@gmail.com>
2023-02-15 16:03:13 +0100

    fix: Implement for scala-2

--- core/src/main/scala-2/caliban/schema/SchemaDerivation.scala
+++ core/src/main/scala-2/caliban/schema/SchemaDerivation.scala
@@ -16,9 +16,10 @@ trait SchemaDerivation[R] extends LowPriorityDerivedSchema {
    * Default naming logic for input types.
    * This is needed to avoid a name clash between a type used as an input and the same type used as an output.
    * GraphQL needs 2 different types, and they can't have the same name.
-   * By default, we add the "Input" suffix after the type name.
+   * By default, the "Input" suffix is added after the type name, given that it is not already present
    */
-  def customizeInputTypeName(name: String): String = s"${name}Input"
+  def customizeInputTypeName(name: String): String =
+    if (name.endsWith("Input")) name else s"${name}Input"
 
   type Typeclass[T] = Schema[R, T]
 
