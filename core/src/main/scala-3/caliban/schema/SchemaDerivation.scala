a6a9e128de011eb7a28f0f977b3221c2c3ae342a
Johan Brodin <johan.brodin@gmail.com>
2023-02-19 03:33:06 +0100

    feat: Only customize names without Input-suffix (#1471)
    * feat: Only customize names without Input-suffix
    
    If a field is already called `FooInput` it will today be changed to `FooInputInput` if I don't explicitly change provide a @GQLInputName annotation.
    
    * fix: Implement for scala-2
    
    * doc: tpyo
    
    Missing dot

--- core/src/main/scala-3/caliban/schema/SchemaDerivation.scala
+++ core/src/main/scala-3/caliban/schema/SchemaDerivation.scala
@@ -28,9 +28,13 @@ trait SchemaDerivation[A] {
    * Default naming logic for input types.
    * This is needed to avoid a name clash between a type used as an input and the same type used as an output.
    * GraphQL needs 2 different types, and they can't have the same name.
-   * By default, we add the "Input" suffix after the type name.
+   * By default, the "Input" suffix is added after the type name, given that it is not already present.
    */
-  def customizeInputTypeName(name: String): String = s"${name}Input"
+  def customizeInputTypeName(name: String): String =
+    name match {
+      case s"${prefix}Input" => name
+      case _                 => s"${name}Input"
+    }
 
   inline def recurse[R, Label, A <: Tuple](
     inline values: List[(String, List[Any], Schema[R, Any], Int)] = Nil
