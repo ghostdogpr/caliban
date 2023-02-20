c1ec02e6bce63fd6945cdf6d8936f09991d30878
frekw <fredrik@warnsberg.se>
2023-02-20 23:36:36 +0100

    make metrics test not recursive

--- core/src/test/scala/caliban/wrappers/FieldMetricsSpec.scala
+++ core/src/test/scala/caliban/wrappers/FieldMetricsSpec.scala
@@ -18,7 +18,7 @@ object FieldMetricsSpec extends ZIOSpecDefault {
   case class Person(
     name: NameArgs => ZIO[Any, Nothing, String],
     age: ZIO[Any, Nothing, Int],
-    friends: ZIO[Any, Nothing, List[Person]] = ZIO.succeed(List(Person("Bob"), Person("Alice"), Person("Joe")))
+    friends: ZIO[Any, Nothing, List[Friend]] = ZIO.succeed(List(Friend("Bob"), Friend("Alice"), Friend("Joe")))
   )
   object Person {
     def apply(name: String): Person = new Person(
@@ -26,7 +26,21 @@ object FieldMetricsSpec extends ZIOSpecDefault {
       age = (Random.nextIntBetween(0, 100) zip Random.nextIntBetween(0, 500)).flatMap { case (i, d) =>
         ZIO.succeed(i).delay(d.milliseconds)
       },
-      friends = randomSleep.as(List(Person("Joe"), Person("Bob"), Person("Alice")))
+      friends = randomSleep.as(List(Friend("Joe"), Friend("Bob"), Friend("Alice")))
+    )
+  }
+
+  case class Friend(
+    name: NameArgs => ZIO[Any, Nothing, String],
+    age: ZIO[Any, Nothing, Int]
+  )
+
+  object Friend {
+    def apply(name: String): Friend = new Friend(
+      name = (args: NameArgs) => ZIO.sleep(args.delay.milliseconds).as(name),
+      age = (Random.nextIntBetween(0, 100) zip Random.nextIntBetween(0, 500)).flatMap { case (i, d) =>
+        ZIO.succeed(i).delay(d.milliseconds)
+      }
     )
   }
 
@@ -64,10 +78,12 @@ object FieldMetricsSpec extends ZIOSpecDefault {
         res         <- fiber.join
         root        <- metric.tagged("field", "Queries.person").value
         name        <- metric.tagged("field", "Person.name").value
+        friendsName <- metric.tagged("field", "Friend.name").value
       } yield assertTrue(res.errors == List.empty) &&
         assertTrue(getCountForDuration(root, 0.5) == 1) &&
         assertTrue(getCountForDuration(name, 0.1) == 1) && // top-level name
-        assertTrue(getCountForDuration(name, 0.5) == 4) // top level + three friends
+        assertTrue(getCountForDuration(name, 0.5) == 1) &&     // top level
+        assertTrue(getCountForDuration(friendsName, 0.5) == 3) // three friends
     }
   )
 }
