b810b9e53d324e0da8da20aa98edb7da67472132
kyri-petrou <kyri.petrou@outlook.com>
2023-03-24 18:26:38 +1100

    Update laminext to 0.15.0

--- client-laminext/src/test/scala/caliban/client/laminext/Main.scala
+++ client-laminext/src/test/scala/caliban/client/laminext/Main.scala
@@ -5,7 +5,7 @@ import org.scalajs.dom
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val _ = documentEvents.onDomContentLoaded.foreach { _ =>
+    val _ = documentEvents(_.onDomContentLoaded).foreach { _ =>
       val appContainer = dom.document.querySelector("#app")
       appContainer.innerHTML = ""
       val _            = render(appContainer, Page.view)
