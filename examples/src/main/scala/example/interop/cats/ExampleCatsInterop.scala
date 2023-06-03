7f05a8cadde2ff5019ea865ba61f5208bac8d1f7
satorg <satorg@gmail.com>
2023-06-02 22:56:05 -0700

    add stream interop to examples

--- examples/src/main/scala/example/interop/cats/ExampleCatsInterop.scala
+++ examples/src/main/scala/example/interop/cats/ExampleCatsInterop.scala
@@ -1,47 +1,93 @@
 package example.interop.cats
 
+import caliban.ResponseValue.{ ObjectValue, StreamValue }
 import caliban._
+import caliban.interop.cats.ToEffect
+import caliban.interop.cats.implicits._
+import caliban.interop.fs2.implicits._
 import caliban.schema.Schema.auto._
+import cats.effect.std.{ Console, Dispatcher, Random }
+import cats.effect.syntax.all._
+import cats.effect.{ ExitCode, IO, IOApp, LiftIO }
+import fs2.Stream
+import zio.ZIO
+import zio.interop.catz._
+import zio.stream.ZStream
+import zio.stream.interop.fs2z._
 
-import cats.effect.{ ExitCode, IO, IOApp }
-import cats.effect.std.Dispatcher
-import zio.Runtime
+import scala.concurrent.duration._
 
-object ExampleCatsInterop extends IOApp {
+object ExampleCatsInterop extends IOApp.Simple {
 
-  import caliban.interop.cats.implicits._
+  implicit val zioRuntime: zio.Runtime[Any] = zio.Runtime.default
 
-  implicit val zioRuntime: Runtime[Any] = Runtime.default
+  val console = Console[IO]
+  val random  = Random.javaUtilConcurrentThreadLocalRandom[IO]
 
   case class Number(value: Int)
 
-  case class Queries(numbers: List[Number], randomNumber: IO[Number])
+  case class Queries(numbers: List[Number], randomNumber: IO[Number], moreRandomNumbers: Stream[IO, Number])
 
-  val numbers      = List(1, 2, 3, 4).map(Number)
-  val randomNumber = IO(scala.util.Random.nextInt()).map(Number)
+  case class Subscriptions(numbers: Stream[IO, Number])
 
-  val queries = Queries(numbers, randomNumber)
+  val numbers             = List(1, 2, 3, 4).map(Number)
+  val randomNumber        = random.nextInt.map(Number)
+  val moreRandomNumbers   = Stream.repeatEval(randomNumber).take(5)
+  val subscriptionNumbers = Stream.emits(5 to 8).covary[IO].spaced(500.millis).map(Number)
 
-  val query = """
-  {
-    numbers {
-      value
-    }
+  val queries       = Queries(numbers, randomNumber, moreRandomNumbers)
+  val subscriptions = Subscriptions(subscriptionNumbers)
 
-    randomNumber {
-      value
-    }
-  }"""
+  val query =
+    """|{
+       |  numbers {
+       |    value
+       |  }
+       |
+       |  randomNumber {
+       |    value
+       |  }
+       |
+       |  moreRandomNumbers {
+       |    value
+       |  }
+       |}
+       |""".stripMargin
 
-  override def run(args: List[String]): IO[ExitCode] =
+  val subscription =
+    """|subscription {
+       |  numbers {
+       |    value
+       |  }
+       |}
+       |""".stripMargin
+
+  override def run: IO[Unit] =
     Dispatcher.parallel[IO].use { implicit dispatcher => // required for a derivation of the schema
-      val api = graphQL(RootResolver(queries))
+      val api = graphQL(RootResolver(Some(queries), Option.empty[Unit], Some(subscriptions)))
 
       for {
         interpreter <- api.interpreterAsync[IO]
-        _           <- interpreter.checkAsync[IO](query)
-        result      <- interpreter.executeAsync[IO](query)
-        _           <- IO(println(result.data))
-      } yield ExitCode.Success
+
+        _      <- console.println("execute a query:")
+        _      <- interpreter.checkAsync[IO](query)
+        result <- interpreter.executeAsync[IO](query)
+        _      <- console.println(result.data)
+
+        _      <- console.println("subscribe to a stream:")
+        _      <- interpreter.checkAsync[IO](subscription)
+        result <- interpreter.executeAsync[IO](subscription)
+
+        _ <- result.data match {
+               case ObjectValue(("numbers", StreamValue(zstream)) :: Nil) =>
+                 zstream.toFs2Stream
+                   .translate(ToEffect[IO, Any].toEffectK)
+                   .foreach(console.println)
+                   .compile
+                   .drain
+
+               case _ => console.println(s"Wrong result: ${result.data}")
+             }
+      } yield ()
     }
 }
