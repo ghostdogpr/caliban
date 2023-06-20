29d1778234961543209b9c5157e737b2cbf3980b
Pierre Ricadat <ghostdogpr@gmail.com>
2023-06-20 21:18:43 +0900

    PoC for gateway

---
+++ examples/src/main/scala/example/mesh/BookApi.scala
@@ -0,0 +1,43 @@
+package example.mesh
+
+import caliban._
+import caliban.interop.tapir.HttpInterpreter
+import caliban.schema.ArgBuilder.auto._
+import caliban.schema.Schema.auto._
+import sttp.tapir.json.circe._
+import zio._
+import zio.http._
+
+object BookApi extends ZIOAppDefault {
+  case class BookArgs(id: String)
+  case class BooksArgs(limit: Int)
+  case class CategoriesArgs(limit: Int)
+  case class Book(authorId: String, categoryId: String, id: String, title: String)
+  case class Category(id: String, name: String)
+  case class Query(book: BookArgs => Book, books: BooksArgs => List[Book], categories: CategoriesArgs => List[Category])
+
+  val categories = List(
+    Category("0", "Fiction"),
+    Category("1", "French")
+  )
+  val books      = List(
+    Book("1", "1", "0", "Illusion Perdues"),
+    Book("0", "0", "1", "Dune")
+  )
+
+  val resolver = RootResolver(
+    Query(args => books.find(_.id == args.id).get, args => books.take(args.limit), args => categories.take(args.limit))
+  )
+  val api      = graphQL(resolver)
+
+  def run: Task[Unit] =
+    api.interpreter
+      .flatMap(interpreter =>
+        Server.serve(
+          Http.collectHttp[Request] { case _ -> !! / "api" / "graphql" =>
+            ZHttpAdapter.makeHttpService(HttpInterpreter(interpreter))
+          }
+        )
+      )
+      .provide(ZLayer.succeed(Server.Config.default.port(8082)), Server.live)
+}
---
+++ examples/src/main/scala/example/mesh/StoreApi.scala
@@ -0,0 +1,47 @@
+package example.mesh
+
+import caliban.Value.StringValue
+import caliban._
+import caliban.interop.tapir.HttpInterpreter
+import caliban.schema.ArgBuilder.auto._
+import caliban.schema.Schema.auto._
+import caliban.schema.{ ArgBuilder, Schema }
+import sttp.tapir.json.circe._
+import zio._
+import zio.http._
+
+object StoreApi extends ZIOAppDefault {
+  case class ID(value: String)
+  case class Store(id: ID, name: String, location: String)
+  case class BookSellArgs(storeId: ID)
+  case class Sells(bookId: ID, sellsCount: Int, monthYear: String, storeId: ID)
+  case class Query(stores: List[Store], bookSells: BookSellArgs => List[Sells])
+
+  val stores = List(
+    Store(ID("0"), "Librairie", "Paris, France"),
+    Store(ID("1"), "Book store", "New York, New York, United States")
+  )
+  val sells  = List(
+    Sells(ID("0"), 1932, "03/22", ID("0")),
+    Sells(ID("1"), 192, "03/22", ID("0")),
+    Sells(ID("0"), 2387, "03/22", ID("1")),
+    Sells(ID("1"), 283, "03/22", ID("1"))
+  )
+
+  implicit val idSchema: Schema[Any, ID]    = Schema.scalarSchema("ID", None, None, None, id => StringValue(id.value))
+  implicit val idArgBuilder: ArgBuilder[ID] = ArgBuilder.string.map(ID)
+
+  val resolver = RootResolver(Query(stores, args => sells.filter(_.storeId == args.storeId)))
+  val api      = graphQL(resolver)
+
+  def run: Task[Unit] =
+    api.interpreter
+      .flatMap(interpreter =>
+        Server.serve(
+          Http.collectHttp[Request] { case _ -> !! / "api" / "graphql" =>
+            ZHttpAdapter.makeHttpService(HttpInterpreter(interpreter))
+          }
+        )
+      )
+      .provide(ZLayer.succeed(Server.Config.default.port(8081)), Server.live)
+}
