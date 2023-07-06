1285a5942801a78de3f4e2d078c8446f3b014a2f
Scala Steward <scala_steward@virtuslab.com>
2023-07-06 19:38:42 +0000

    Reformat with scalafmt 3.7.7
    Executed command: scalafmt --non-interactive

--- core/src/test/scala/caliban/schema/SchemaSpec.scala
+++ core/src/test/scala/caliban/schema/SchemaSpec.scala
@@ -266,7 +266,7 @@ object SchemaSpec extends ZIOSpecDefault {
 
   @GQLInterface
   sealed trait MyInterface
-  object MyInterface   {
+  object MyInterface {
     case class A(common: Int, different: String)  extends MyInterface
     case class B(common: Int, different: Boolean) extends MyInterface
   }
@@ -281,7 +281,7 @@ object SchemaSpec extends ZIOSpecDefault {
   @GQLUnion
   sealed trait RedirectingUnion
 
-  object RedirectingUnion  {
+  object RedirectingUnion {
     case class B(common: Int)
 
     case class A(common: Int) extends RedirectingUnion
