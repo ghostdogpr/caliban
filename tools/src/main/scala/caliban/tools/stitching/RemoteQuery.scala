bab5209aee31865133aa45ca648ed4bfe3bc707f
Pierre Ricadat <pierre.ricadat@devsisters.com>
2023-08-25 15:14:08 +0900

    Fix logic

--- tools/src/main/scala/caliban/tools/stitching/RemoteQuery.scala
+++ tools/src/main/scala/caliban/tools/stitching/RemoteQuery.scala
@@ -4,7 +4,7 @@ import caliban._
 import caliban.execution.Field
 import caliban.parsing.SourceMapper
 import caliban.parsing.adt.Definition.ExecutableDefinition.OperationDefinition
-import caliban.parsing.adt.{ Document, OperationType }
+import caliban.parsing.adt.{ Document, OperationType, Selection }
 import caliban.rendering.DocumentRenderer
 
 case class RemoteQuery(field: Field) { self =>
@@ -43,9 +43,21 @@ object RemoteQuery {
           None,
           Nil,
           Nil,
-          field.fields.map(_.toSelection)
+          if (field.parentType.isEmpty) field.fields.map(f => addTypeName(f.toSelection))
+          else List(addTypeName(field.toSelection))
         )
       ),
       SourceMapper.empty
     )
+
+  private def addTypeName(selection: Selection): Selection =
+    selection match {
+      case f: Selection.Field =>
+        if (f.selectionSet.isEmpty) f
+        else
+          f.copy(selectionSet =
+            Selection.Field(None, "__typename", Map.empty, Nil, Nil, 0) :: f.selectionSet.map(addTypeName)
+          )
+      case other              => other
+    }
 }
