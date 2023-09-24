ff4c806e43f6715fba0809c39cc0bc3e1e2bbb30
Paul Daniels <paul@leadiq.com>
2023-09-24 08:27:06 +0800

    add federation 2.5 support (#1906)

--- federation/src/main/scala/caliban/federation/package.scala
+++ federation/src/main/scala/caliban/federation/package.scala
@@ -1,6 +1,6 @@
 package caliban
 
-import caliban.federation.v2x.{ FederationDirectivesV2_3, FederationV2, Versions }
+import caliban.federation.v2x.{ FederationDirectivesV2_3, FederationDirectivesV2_5, FederationV2, Versions }
 
 package object federation {
 
@@ -8,5 +8,7 @@ package object federation {
   lazy val v2_0 = new FederationV2(List(Versions.v2_0))
   lazy val v2_1 = new FederationV2(List(Versions.v2_1))
   lazy val v2_3 = new FederationV2(List(Versions.v2_3)) with FederationDirectivesV2_3
+  lazy val v2_4 = new FederationV2(List(Versions.v2_4)) with FederationDirectivesV2_3
+  lazy val v2_5 = new FederationV2(List(Versions.v2_5)) with FederationDirectivesV2_5
 
 }
