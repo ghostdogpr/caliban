be4ff0db88ee39b65160670a459d9ccecefee24b
Pierre Ricadat <pierre.ricadat@devsisters.com>
2023-10-06 16:20:35 +0900

    Polish

---
+++ tools/src/main/scala/caliban/tools/gateway/Resolver.scala
@@ -0,0 +1,44 @@
+package caliban.tools.gateway
+
+import caliban.ResponseValue.ObjectValue
+import caliban.{ InputValue, ResponseValue }
+
+sealed trait Resolver
+object Resolver {
+  case class Extract(extract: ObjectValue => ResponseValue, fields: List[Field] = Nil) extends Resolver
+  case class Fetch(
+    subGraph: String,
+    sourceFieldName: String,
+    fields: List[Field],
+    argumentMappings: Map[String, InputValue => (String, InputValue)] = Map.empty,
+    filterBatchResults: Option[(Map[String, InputValue], ResponseValue) => Boolean] = None
+  ) extends Resolver
+
+  case class Field(
+    name: String,
+    resolver: Resolver,
+    outputName: String,
+    arguments: Map[String, InputValue] = Map.empty
+  )
+
+  object Field {
+    def apply(field: caliban.execution.Field): Resolver.Field =
+      Resolver.Field(
+        field.definition.fold(identity[String] _)(_.renameInput)(field.name),
+        field.definition.flatMap(_.extend) match {
+          case Some(extend) =>
+            Resolver.Fetch(
+              extend.sourceGraph,
+              sourceFieldName = extend.sourceFieldName,
+              fields = field.fields.map(apply),
+              argumentMappings = extend.argumentMappings,
+              filterBatchResults = extend.filterBatchResults
+            )
+          case None         =>
+            Resolver.Extract(if (field.isRoot) identity else _.get(field.name), field.fields.map(apply))
+        },
+        field.alias.getOrElse(field.name),
+        field.arguments.map { case (k, v) => field.definition.fold(identity[String] _)(_.renameArguments)(k) -> v }
+      )
+  }
+}
---
+++ tools/src/main/scala/caliban/tools/gateway/SuperGraphExecutor.scala
@@ -0,0 +1,123 @@
+package caliban.tools.gateway
+
+import caliban.CalibanError.ExecutionError
+import caliban.{ CalibanError, GraphQL, GraphQLResponse, ResponseValue }
+import caliban.ResponseValue.{ ListValue, ObjectValue }
+import caliban.Value.NullValue
+import caliban.execution.{ ExecutionRequest, Executor, Feature, Field, QueryExecution }
+import caliban.introspection.adt.{ __Directive, Extend, TypeVisitor }
+import caliban.schema.Step.NullStep
+import caliban.schema.{ Operation, RootSchemaBuilder, Types }
+import caliban.tools.gateway.FetchDataSource.FetchRequest
+import caliban.tools.gateway.SubGraph.SubGraphExecutor
+import caliban.wrappers.Wrapper
+import caliban.wrappers.Wrapper.FieldWrapper
+import zio.{ Chunk, URIO }
+import zio.prelude.NonEmptyList
+import zio.query.ZQuery
+
+private case class SuperGraphExecutor[-R](
+  private val subGraphs: NonEmptyList[SubGraphExecutor[R]],
+  private val transformers: Chunk[TypeVisitor]
+) extends GraphQL[R] {
+  private val subGraphMap: Map[String, SubGraphExecutor[R]] = subGraphs.map(g => g.name -> g).toMap
+
+  protected val schemaBuilder: RootSchemaBuilder[R] = {
+    val builder = subGraphs.collect {
+      case subGraph if subGraph.exposeAtRoot =>
+        RootSchemaBuilder(
+          Some(Operation(subGraph.schema.queryType, NullStep)),
+          subGraph.schema.mutationType.map(mutation => Operation(mutation, NullStep)),
+          subGraph.schema.subscriptionType.map(subscription => Operation(subscription, NullStep))
+        ).visit(
+          TypeVisitor.fields.modifyWith((t, field) =>
+            if (t.name == subGraph.schema.queryType.name)
+              field.copy(extend = Some(Extend(subGraph.name, field.name, Map.empty, None)))
+            else field
+          // TODO mutation and subscription
+          )
+        )
+    }.reduceLeft(_ |+| _)
+    transformers.foldLeft(builder) { case (builder, transformer) => builder.visit(transformer) }
+  }
+
+  protected val wrappers: List[Wrapper[R]]              = Nil
+  protected val additionalDirectives: List[__Directive] = Nil
+  protected val features: Set[Feature]                  = Set.empty
+
+  protected override def resolve[R1 <: R](
+    op: Operation[R1],
+    fieldWrappers: List[FieldWrapper[R1]], // TODO field wrappers
+    isIntrospection: Boolean
+  )(req: ExecutionRequest): URIO[R1, GraphQLResponse[CalibanError]] =
+    if (isIntrospection)
+      Executor.executeRequest(req, op.plan, fieldWrappers, QueryExecution.Parallel, features)
+    else
+      resolveField(Resolver.Field(req.field), NullValue)
+        .fold(
+          error => GraphQLResponse(NullValue, List(error)),
+          result => GraphQLResponse(result, Nil)
+        )
+        .run
+
+  private def resolveField(field: Resolver.Field, parent: ResponseValue): ZQuery[R, ExecutionError, ResponseValue] =
+    field.resolver match {
+      case Resolver.Extract(extract, children)                                                           =>
+        extract(parent.asObjectValue.getOrElse(ObjectValue(Nil))) match {
+          case res @ ObjectValue(fields) =>
+            ZQuery
+              .foreachBatched(children)(child => resolveField(child, res).map(child -> _))
+              .map(children =>
+                res.copy(fields = fields ++ children.map { case (field, response) =>
+                  field.outputName -> response
+                })
+              )
+          case other                     => ZQuery.succeed(other)
+        }
+      case Resolver.Fetch(subGraph, sourceFieldName, fields, argumentMappings, mapBatchResultToArgument) =>
+        subGraphMap.get(subGraph) match {
+          case Some(subGraph) =>
+            val arguments = field.arguments ++ argumentMappings.map { case (k, f) =>
+              f(parent.asObjectValue.map(_.get(k)).map(_.toInputValue).getOrElse(NullValue))
+            }.filterNot { case (_, v) => v == NullValue }
+            ZQuery
+              .fromRequest(
+                FetchRequest(
+                  subGraph,
+                  sourceFieldName,
+                  fields
+                    .flatMap(f =>
+                      f.resolver match {
+                        case _: Resolver.Extract                          => Set(f.name)
+                        case Resolver.Fetch(_, _, _, argumentMappings, _) => argumentMappings.keySet
+                      }
+                    )
+                    .distinct
+                    .map(name => Field(name, Types.string, None)),
+                  arguments
+                )
+              )(FetchDataSource[R]) // TODO don't make new datasource for each request
+              .map(_.asObjectValue.map(_.get(sourceFieldName)).getOrElse(NullValue))
+              .map(value =>
+                value.asListValue
+                  .map(_.filter(v => mapBatchResultToArgument.fold(true)(_(arguments, v))))
+                  .getOrElse(value)
+              )
+              .flatMap {
+                case ListValue(values) =>
+                  ZQuery
+                    .foreachBatched(values)(value =>
+                      ZQuery
+                        .foreachBatched(fields)(field => resolveField(field, value).map(field.outputName -> _))
+                        .map(ObjectValue.apply)
+                    )
+                    .map(ListValue.apply)
+                case value             =>
+                  ZQuery
+                    .foreachBatched(fields)(field => resolveField(field, value).map(field.outputName -> _))
+                    .map(ObjectValue.apply)
+              }
+          case None           => ZQuery.fail(ExecutionError(s"Subgraph $subGraph not found"))
+        }
+    }
+}
