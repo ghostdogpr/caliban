f59ab2ebdd977a8f8ea386e7cf7acc1aa2ea8f3f
Pierre Ricadat <ghostdogpr@gmail.com>
2023-10-20 14:43:04 +0200

    Format

--- core/src/main/scala-3/caliban/schema/macros/Macros.scala
+++ core/src/main/scala-3/caliban/schema/macros/Macros.scala
@@ -30,7 +30,7 @@ object Macros {
       tpe.typeSymbol.primaryConstructor.paramSymss.flatten.map { field =>
         Expr(field.name) -> field.annotations.filter { a =>
           a.tpe.typeSymbol.maybeOwner.isNoSymbol ||
-            (a.tpe.typeSymbol.owner.fullName != "scala.annotation.internal" && a.tpe.typeSymbol.owner.fullName != "jdk.internal")
+          (a.tpe.typeSymbol.owner.fullName != "scala.annotation.internal" && a.tpe.typeSymbol.owner.fullName != "jdk.internal")
         }.map(_.asExpr.asInstanceOf[Expr[Any]])
       }.filter(_._2.nonEmpty).map((name, anns) => Expr.ofTuple(name, Expr.ofList(anns)))
     }
