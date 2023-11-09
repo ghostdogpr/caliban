590dbf92c78708fe2e672b15332c8efef346d105
Kyri Petrou <kyri.petrou@outlook.com>
2023-11-09 19:15:55 +1100

    Split subschemas into separate files

---
+++ core/src/main/scala-3/caliban/schema/SumSchema.scala
@@ -0,0 +1,43 @@
+package caliban.schema
+
+import caliban.introspection.adt.__Type
+import caliban.schema.Annotations.{ GQLInterface, GQLUnion }
+import caliban.schema.DerivationUtils.*
+import caliban.schema.Types.makeUnion
+import magnolia1.TypeInfo
+
+final private class SumSchema[R, A](
+  _members: => List[(String, List[Any], Schema[R, Any])],
+  info: TypeInfo,
+  annotations: List[Any]
+)(ordinal: A => Int)
+    extends Schema[R, A] {
+  private lazy val members = _members
+
+  private lazy val subTypes = members.map { (label, subTypeAnnotations, schema) =>
+    (label, schema.toType_(), subTypeAnnotations)
+  }.sortBy(_._1)
+
+  private lazy val schemas = members.map(_._3).toVector // Vector's .apply is O(1) vs List's O(N)
+
+  private lazy val isEnum = subTypes.forall((_, t, _) => t.allFields.isEmpty && t.allInputFields.isEmpty)
+  private val isInterface = annotations.contains(GQLInterface())
+  private val isUnion     = annotations.contains(GQLUnion())
+
+  def toType(isInput: Boolean, isSubscription: Boolean): __Type =
+    if (!isInterface && !isUnion && subTypes.nonEmpty && isEnum) mkEnum(annotations, info, subTypes)
+    else if (!isInterface)
+      makeUnion(
+        Some(getName(annotations, info)),
+        getDescription(annotations),
+        subTypes.map(_._2).distinctBy(_.name).map(fixEmptyUnionObject),
+        Some(info.full),
+        Some(getDirectives(annotations))
+      )
+    else {
+      val impl = subTypes.map(_._2.copy(interfaces = () => Some(List(toType(isInput, isSubscription)))))
+      mkInterface(annotations, info, impl)
+    }
+
+  def resolve(value: A): Step[R] = schemas(ordinal(value)).resolve(value)
+}
---
+++ core/src/main/scala-3/caliban/schema/ValueTypeSchema.scala
@@ -0,0 +1,22 @@
+package caliban.schema
+
+import caliban.introspection.adt.__Type
+import caliban.schema.Annotations.GQLValueType
+import caliban.schema.DerivationUtils.*
+import caliban.schema.Types.makeScalar
+import magnolia1.TypeInfo
+
+final private class ValueTypeSchema[R, A](
+  _schema: => Schema[R, Any],
+  info: TypeInfo,
+  anns: List[Any]
+) extends Schema[R, A] {
+  private val name        = getName(anns, info)
+  private lazy val schema = _schema
+
+  def toType(isInput: Boolean, isSubscription: Boolean): __Type =
+    if (anns.contains(GQLValueType(true))) makeScalar(name, getDescription(anns))
+    else schema.toType_(isInput, isSubscription)
+
+  def resolve(value: A): Step[R] = schema.resolve(value.asInstanceOf[Product].productElement(0))
+}
