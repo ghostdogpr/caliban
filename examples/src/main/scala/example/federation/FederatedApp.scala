e65cbd0d1ff91f96977b67c66444b1bcbbc18af7
Paul <paulpdaniels@gmail.com>
2023-12-16 15:20:09 +0800

    add federation 2.6 support, update gateway dependencies

--- examples/src/main/scala/example/federation/FederatedApp.scala
+++ examples/src/main/scala/example/federation/FederatedApp.scala
@@ -1,75 +1,19 @@
 package example.federation
 
+import caliban.quick._
 import example.federation.FederationData.characters.sampleCharacters
 import example.federation.FederationData.episodes.sampleEpisodes
-import caliban.Http4sAdapter
-import caliban.interop.tapir.HttpInterpreter
-import cats.data.Kleisli
-import com.comcast.ip4s._
-import fs2.io.net.Network
-import org.http4s.StaticFile
-import org.http4s.implicits._
-import org.http4s.server.Router
-import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.middleware.CORS
 import zio._
-import zio.interop.catz._
 
-object FederatedApp extends CatsApp {
-  import sttp.tapir.json.circe._
+object FederatedApp extends ZIOAppDefault {
 
-  type ExampleTask[A] = RIO[Any, A]
-
-  private implicit val network: Network[ExampleTask] = Network.forAsync
-
-  val service1 =
-    CharacterService
-      .make(sampleCharacters)
-      .memoize
-      .flatMap(layer =>
-        for {
-          interpreter <- FederatedApi.Characters.api.interpreter.map(_.provideLayer(layer))
-          _           <- EmberServerBuilder
-                           .default[ExampleTask]
-                           .withHost(host"localhost")
-                           .withPort(port"8089")
-                           .withHttpApp(
-                             Router[ExampleTask](
-                               "/api/graphql" -> CORS.policy(Http4sAdapter.makeHttpService(HttpInterpreter(interpreter))),
-                               "/graphiql"    -> Kleisli.liftF(StaticFile.fromResource("/graphiql.html", None))
-                             ).orNotFound
-                           )
-                           .build
-                           .toScopedZIO
-          _           <- Console.printLine("Server online at http://localhost:8089/\nPress RETURN to stop...")
-          _           <- Console.readLine
-        } yield ()
-      )
-
-  val service2 =
-    EpisodeService
-      .make(sampleEpisodes)
-      .memoize
-      .flatMap(layer =>
-        for {
-          interpreter <- FederatedApi.Episodes.api.interpreter.map(_.provideLayer(layer))
-          _           <- EmberServerBuilder
-                           .default[ExampleTask]
-                           .withHost(host"localhost")
-                           .withPort(port"8088")
-                           .withHttpApp(
-                             Router[ExampleTask](
-                               "/api/graphql" -> CORS.policy(Http4sAdapter.makeHttpService(HttpInterpreter(interpreter))),
-                               "/graphiql"    -> Kleisli.liftF(StaticFile.fromResource("/graphiql.html", None))
-                             ).orNotFound
-                           )
-                           .build
-                           .toScopedZIO
-          _           <- Console.printLine("Server online at http://localhost:8088/\nPress RETURN to stop...")
-          _           <- Console.readLine
-        } yield ()
-      )
+  val episodesApi   = FederatedApi.Episodes.api.runServer(8089, "/api/graphql", Some("/graphiql"))
+  val charactersApi = FederatedApi.Characters.api.runServer(8088, "/api/graphql", Some("/graphiql"))
 
   override def run =
-    (service1 race service2).exitCode
+    (episodesApi zipPar charactersApi)
+      .provide(
+        EpisodeService.make(sampleEpisodes),
+        CharacterService.make(sampleCharacters)
+      )
 }
