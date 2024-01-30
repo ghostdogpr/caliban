8bdd1f18d053973439a32ace855e6e144d455987
Kyri Petrou <kyri.petrou@outlook.com>
2024-01-30 21:26:49 +1100

    Avoid unnecessary fragment validations

--- core/src/main/scala/caliban/validation/Validator.scala
+++ core/src/main/scala/caliban/validation/Validator.scala
@@ -444,14 +444,20 @@ object Validator {
     }
   }
 
+  private def containsFragments(selectionSet: List[Selection]): Boolean =
+    selectionSet.exists {
+      case f: Selection.Field => containsFragments(f.selectionSet)
+      case _                  => true
+    }
+
   private def validateSelectionSet(
     context: Context,
     selectionSet: List[Selection],
     currentType: __Type
   ): EReader[Any, ValidationError, Unit] = {
     val v1 = validateFields(context, selectionSet, currentType)
-    val v2 = FragmentValidator.findConflictsWithinSelectionSet(context, context.rootType.queryType, selectionSet)
-    v1 *> v2
+    def v2 = FragmentValidator.findConflictsWithinSelectionSet(context, context.rootType.queryType, selectionSet)
+    if (context.fragments.nonEmpty || containsFragments(selectionSet)) v1 *> v2 else v1
   }
 
   private def validateFields(
