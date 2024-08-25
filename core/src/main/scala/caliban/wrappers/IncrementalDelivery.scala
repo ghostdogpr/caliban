e515bf9cff42722acdf3b204e73389000ffb0962
Paul <paulpdaniels@gmail.com>
2024-08-25 16:55:18 +0800

    implement validation

--- core/src/main/scala/caliban/wrappers/IncrementalDelivery.scala
+++ core/src/main/scala/caliban/wrappers/IncrementalDelivery.scala
@@ -1,15 +1,15 @@
 package caliban.wrappers
 
-import caliban.{ CalibanError, Configurator, GraphQL, GraphQLAspect, InputValue, Value }
 import caliban.execution.{ ExecutionRequest, Feature }
 import caliban.introspection.adt.{ __Directive, __DirectiveLocation, __InputValue, __Type }
-import caliban.parsing.SourceMapper
-import caliban.parsing.adt.Selection.{ Field, FragmentSpread }
-import caliban.parsing.adt.{ Directive, Directives, Document, OperationType, Selection, VariableDefinition }
+import caliban.parsing.adt.Definition.ExecutableDefinition.FragmentDefinition
+import caliban.parsing.adt.Selection.{ Field, FragmentSpread, InlineFragment }
+import caliban.parsing.adt._
 import caliban.schema.Types
 import caliban.validation.ValidationOps.validateAllDiscard
-import caliban.validation.Validator.QueryValidation
+import caliban.validation.Validator.{ failValidation, QueryValidation }
 import caliban.wrappers.Wrapper.ValidationWrapper
+import caliban._
 import zio.ZIO
 
 import scala.annotation.tailrec
@@ -19,7 +19,9 @@ object IncrementalDelivery {
 
   private[caliban] val deferDirective = __Directive(
     Directives.Defer,
-    Some(""),
+    Some(
+      "Marks a fragment as being optionally deferrable. Allowing the backend to split the query and return non-deferred parts first. This implicitly uses a streaming transport protocol which requires client support."
+    ),
     Set(__DirectiveLocation.FRAGMENT_SPREAD, __DirectiveLocation.INLINE_FRAGMENT),
     _ =>
       List(__InputValue("if", None, () => Types.boolean, None), __InputValue("label", None, () => Types.string, None)),
@@ -27,8 +29,10 @@ object IncrementalDelivery {
   )
 
   private[caliban] val streamDirective = __Directive(
-    "stream",
-    Some(""),
+    Directives.Stream,
+    Some(
+      "Marks a field as being optionally streamable. Allowing the backend to split the returned list value into separate payloads, only returning the first part of the list immediately. This implicitly uses a streaming transport protocol which requires client support."
+    ),
     Set(__DirectiveLocation.FIELD),
     _ =>
       List(
@@ -69,54 +73,60 @@ object IncrementalDelivery {
   }
 
   private val appearsOnlyOnLists: QueryValidation = context => {
-    var error: CalibanError.ValidationError            = null
     val checked: mutable.Set[(String, Option[String])] = mutable.Set.empty
-    val iter                                           = context.operations.iterator
 
-    def validateFields(selectionSet: List[Selection], currentType: __Type) =
+    def validateFields(selectionSet: List[Selection], currentType: __Type): Either[CalibanError.ValidationError, Unit] =
       validateAllDiscard(selectionSet) {
-        case f: Field                         =>
+        case f: Field                                          =>
           validateField(f, currentType)
-        case FragmentSpread(name, directives) =>
-          context.fragments.getOrElse(name, null) match {
-            case null                                              => Left(CalibanError.ValidationError(s"Fragment $name not found", ""))
-            case fragment if checked.add((name, currentType.name)) =>
-              validateSpread()
+        case FragmentSpread(name, directives)                  =>
+          if (directives.exists(_.name == Directives.Stream))
+            Left(CalibanError.ValidationError("Stream directive was used on a fragment spread", ""))
+          else {
+            context.fragments.getOrElse(name, null) match {
+              case null                                              => Left(CalibanError.ValidationError(s"Fragment $name not found", ""))
+              case fragment if checked.add((name, currentType.name)) =>
+                validateSpread(fragment, currentType)
+              case _                                                 => Right(())
+            }
+          }
+        case InlineFragment(typeCondition, dirs, selectionSet) =>
+          if (dirs.exists(_.name == Directives.Stream))
+            Left(CalibanError.ValidationError("Stream directive was used on an inline fragment", ""))
+          else {
+            if (typeCondition.exists(_.name.contains(currentType.typeNameRepr)))
+              validateFields(selectionSet, currentType)
+            else Right(())
           }
 
       }
 
-    def validateSpread()
+    def validateSpread(fragment: FragmentDefinition, currentType: __Type): Either[CalibanError.ValidationError, Unit] =
+      validateFields(fragment.selectionSet, currentType)
 
-    def validateField(field: Field, currentType: __Type): Either[CalibanError.ValidationError, Unit] = {}
+    def validateField(field: Field, currentType: __Type): Either[CalibanError.ValidationError, Unit] = {
+      val selected = currentType.allFields.find(_.name == field.name)
+      Either.cond(
+        selected.isDefined && !selected.get._type.isList && field.directives.forall(_.name != Directives.Stream),
+        (),
+        CalibanError.ValidationError("Stream directive was used on a non-list field", "")
+      )
+    }
 
-//    @tailrec
-//    def loop(selectionSet: List[Selection], currentType: __Type): Boolean = fields match {
-//      case field :: rest =>
-//        !field.fieldType.isList && field.directives.exists(_.name == Directives.Stream) || loop(field.fields ++ rest)
-//      case Nil           =>
-//        false
-//    }
-//
-//    while (iter.hasNext && (error eq null)) {
-//      val op    = iter.next()
-//      val field = Field(
-//        op.selectionSet,
-//        context.fragments,
-//        Map.empty[String, InputValue],
-//        List.empty[VariableDefinition],
-//        context.rootType.queryType,
-//        SourceMapper.empty,
-//        Nil,
-//        context.rootType
-//      )
-//
-//      if (loop(List(field))) {
-//        error = CalibanError.ValidationError("Stream directive was used on a non-list field", "")
-//      }
-//    }
-
-    if (error ne null) Left(error) else Right(())
+    validateAllDiscard(context.operations) { op =>
+      op.operationType match {
+        case OperationType.Query        =>
+          validateFields(op.selectionSet, context.rootType.queryType)
+        case OperationType.Mutation     =>
+          context.rootType.mutationType.fold[Either[CalibanError.ValidationError, Unit]](
+            failValidation("Mutation operations are not supported on this schema.", "")
+          )(validateFields(op.selectionSet, _))
+        case OperationType.Subscription =>
+          context.rootType.subscriptionType.fold[Either[CalibanError.ValidationError, Unit]](
+            failValidation("Subscription operations are not supported on this schema.", "")
+          )(validateFields(op.selectionSet, _))
+      }
+    }
   }
 
   private val uniqueLabels: QueryValidation = context => {
@@ -164,7 +174,7 @@ object IncrementalDelivery {
     List(onlyTopLevelQuery, uniqueLabels) ++ streamValidations
   }
 
-  private def withValidations(features: Set[Feature]): GraphQLAspect[Nothing, Any] = new ValidationWrapper[Any] {
+  private def withValidations(features: Set[Feature]): Wrapper[Any] = new ValidationWrapper[Any] {
     private val validations = additionalValidations(features)
 
     override def wrap[R1 <: Any](
@@ -176,17 +186,17 @@ object IncrementalDelivery {
   }
 
   def aspect(feature: Feature, others: Feature*): GraphQLAspect[Nothing, Any] = new GraphQLAspect[Nothing, Any] {
-    private val featureSet = Set(feature) ++ others
+    private val featureSet    = Set(feature) ++ others
+    private val directiveList = List(
+      Some(deferDirective).filter(_ => featureSet(Feature.Defer)),
+      Some(streamDirective).filter(_ => featureSet(Feature.Stream))
+    ).flatten
 
     override def apply[R](gql: GraphQL[R]): GraphQL[R] =
-      (gql @@ withValidations(featureSet))
+      gql
         .enableAll(featureSet)
-        .withAdditionalDirectives(
-          List(
-            Some(deferDirective).filter(_ => featureSet(Feature.Defer)),
-            Some(streamDirective).filter(_ => featureSet(Feature.Stream))
-          ).flatten
-        )
+        .withAdditionalDirectives(directiveList)
+        .withWrapper(withValidations(featureSet))
   }
 
 }
