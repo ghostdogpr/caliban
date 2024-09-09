02d14d339ca25fd22faf205b96d66f9ea46e3011
Ivar Ness <ivar@develeon.com>
2024-09-09 12:26:43 +0200

    Added snapshotTest for reuse

--- codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/README.md
+++ codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/README.md
@@ -1,3 +1,14 @@
 # Test doc
 
-This test project has been copied from: https://github.com/guizmaii/poc_compile_time_caliban_client_generation
+This test project has been copied from:
+https://github.com/guizmaii/poc_compile_time_caliban_client_generation
+
+### Running locally
+You can run these tests using following sbt commandos:
+
+```sbt
+project codegenSbt
+++2.12.20
+update
+scripted compiletime-codegen/test-compile
+```
--- codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/modules/posts/src/test/scala/ValidateGraphQlSpec.scala
+++ codegen-sbt/src/sbt-test/compiletime-codegen/test-compile/modules/posts/src/test/scala/ValidateGraphQlSpec.scala
@@ -1,24 +1,24 @@
 import poc.caliban.posts.GraphQLApi
 
 import scala.io.Source
-import java.io.File
-import java.nio.file.{Files, Paths}
 import zio.test.Assertion._
 import zio.test._
+import caliban.tools._
+import java.nio.file.Path
 
-class ValidateGraphQlSpec extends ZIOSpecDefault {
+object ValidateGraphQlSpec extends SnapshotTest {
+  override val testName: String = "ValidateGraphQlSpec"
+
+  val graphqlFile= "src/sbt-test/compiletime-codegen/test-compile/modules/posts/src/test/resources/postservice.graphql"
+  val projectDir = sys.props.get("project.dir").getOrElse("")
 
   override def spec =
     suite("Validate Postservice")(
       test("Render postservice as earlier") {
-        val filename = "postservice.graphql"
-        val expectedGraphQL: String = Source.fromResource(filename).getLines().mkString("\n")
         val gqlApi = GraphQLApi.api
         val renderContent: String = s"${gqlApi.render}"
 
-        //Files.writeString(Paths.get(File(s"/tmp/$filename").toURI), renderContent)
-
-        assertTrue(expectedGraphQL == renderContent)
+        writeAndCompare(Path.of(projectDir).resolve(graphqlFile), renderContent, "Render postservice")
       }
     )
 }
