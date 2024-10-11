5e3ad95c7ff844da4e0c7a1e22f23a041ff458c8
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2024-10-11 10:09:29 +0900

    Make jsoniter codecs public (#2436)

--- core/src/main/scala/caliban/GraphQLWSOutput.scala
+++ core/src/main/scala/caliban/GraphQLWSOutput.scala
@@ -7,7 +7,7 @@ import com.github.plokhotnyuk.jsoniter_scala.macros._
 case class GraphQLWSOutput(`type`: String, id: Option[String], payload: Option[ResponseValue])
 
 object GraphQLWSOutput {
-  private[caliban] implicit val jsoniterCodec: JsonValueCodec[GraphQLWSOutput] = JsonCodecMaker.make
+  implicit val jsoniterCodec: JsonValueCodec[GraphQLWSOutput] = JsonCodecMaker.make
 
   implicit def tapirSchema[F[_]: IsTapirSchema]: F[GraphQLWSOutput] =
     caliban.interop.tapir.schema.wsOutputSchema.asInstanceOf[F[GraphQLWSOutput]]
