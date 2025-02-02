4775723ef2de5556fbbcd34de6c867edd77e58ed
Paul <paulpdaniels@gmail.com>
2025-02-02 18:34:09 +0800

    Attempt to fix compile

--- federation/src/main/scala/caliban/federation/v2x/FederationV2.scala
+++ federation/src/main/scala/caliban/federation/v2x/FederationV2.scala
@@ -1,12 +1,46 @@
 package caliban.federation.v2x
 
+import caliban.GraphQL
 import caliban.federation.{ FederationDirectives, FederationSupport }
+import caliban.rendering.{ DocumentRenderer, Renderer }
 
-class FederationV2(private[v2x] val extensions: List[Extension])
+class FederationV2(extensions: List[Extension])
     extends FederationSupport(Nil, extensions.map(_.toDirective))
     with FederationDirectives
-    with FederationDirectivesV2
-    with FederationRenderingV2
+    with FederationDirectivesV2 {
+
+  /**
+   * Constructs a renderer that can render a GraphQL schema with federation directives.
+   *
+   * This is useful if you need to render the schema to a string or file in CI or for debugging purposes.
+   *
+   * @note Make sure to use this renderer on the graph _before_ apply the federation aspect, otherwise it will include
+   *       the federation specific fields as well.
+   *
+   * @see [[renderFederatedCompact]] when you want to render a compact version of the schema.
+   */
+  def renderFederated[R](graphql: GraphQL[R]): String =
+    renderer[R].render(graphql)
+
+  /**
+   * Constructs a renderer that can render a GraphQL schema with federation directives in a compact format.
+   *
+   * This is useful if you need to render the schema to a string or file in CI or for debugging purposes.
+   *
+   * @note Make sure to use this renderer on the graph _before_ apply the federation aspect, otherwise it will include
+   *       the federation specific fields as well.
+   *
+   * @see [[renderFederated]] when you want to render the schema in pretty form.
+   */
+  def renderFederatedCompact[R](graphql: GraphQL[R]): String =
+    renderer[R].renderCompact(graphql)
+
+  private def renderer[R]: Renderer[GraphQL[R]] =
+    DocumentRenderer
+      .contramap[GraphQL[_]] {
+        _.withSchemaDirectives(extensions.map(_.toDirective)).toDocument
+      }
+}
 
 object FederationV2 {
 
--- federation/src/test/scala/caliban/federation/v2x/FederationV2Spec.scala
+++ federation/src/test/scala/caliban/federation/v2x/FederationV2Spec.scala
@@ -18,20 +18,6 @@ import zio.test._
 import java.util.UUID
 
 object FederationV2Spec extends ZIOSpecDefault {
-  object SchemaWithEntity {
-    import caliban.federation.v2_3._
-
-    @GQLKey("id")
-    @GQLShareable
-    case class Entity(
-      id: UUID
-    )
-
-    case class Query(
-      hello: String,
-      entity: Entity
-    )
-  }
 
   override def spec =
     suite("FederationV2Spec")(
@@ -252,18 +238,28 @@ object FederationV2Spec extends ZIOSpecDefault {
       },
       test("renderer renders the schema including the extensions") {
         import caliban.federation.v2_3._
-        import SchemaWithEntity.{ Query, Entity }
+
+        @GQLKey("id")
+        @GQLShareable
+        case class User(
+          id: UUID
+        )
+
+        case class Query(
+          hello: String,
+          user: User
+        )
 
         val api = graphQL(
           RootResolver(
-            Query(hello = "Hello World!", entity = Entity(UUID.randomUUID()))
+            Query(hello = "Hello World!", user = User(UUID.randomUUID()))
           )
         )
 
         assertTrue(
           renderFederatedCompact(
             api
-          ) == """schema@link(url:"https://specs.apollo.dev/federation/v2.3",import:["@key","@requires","@provides","@external","@shareable","@tag","@inaccessible","@override","@extends","@composeDirective","@interfaceObject"]){query:Query}type Entity@key(fields:"id")@shareable{id:ID!}type Query{hello:String! entity:Entity!}"""
+          ) == """schema@link(url:"https://specs.apollo.dev/federation/v2.3",import:["@key","@requires","@provides","@external","@shareable","@tag","@inaccessible","@override","@extends","@composeDirective","@interfaceObject"]){query:Query}type Query{hello:String! user:User!}type User@key(fields:"id")@shareable{id:ID!}"""
         )
 
       }
