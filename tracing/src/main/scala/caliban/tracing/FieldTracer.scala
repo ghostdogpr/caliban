4f1fc0d9297e4a2242c0a621a945e7da7d7b594d
li-nkSN <link@li-nk.org>
2025-05-03 00:54:18 -0700

    scalafmt

--- tracing/src/main/scala/caliban/tracing/FieldTracer.scala
+++ tracing/src/main/scala/caliban/tracing/FieldTracer.scala
@@ -1,19 +1,19 @@
 package caliban.tracing
 
-import caliban.{ CalibanError, ResponseValue }
+import caliban.{CalibanError, ResponseValue}
 import caliban.execution.FieldInfo
 import caliban.wrappers.Wrapper.FieldWrapper
 import io.opentelemetry.api.trace.StatusCode
 import zio._
 import zio.query.ZQuery
-import zio.telemetry.opentelemetry.trace.{ StatusMapper, Tracer }
+import zio.telemetry.opentelemetry.trace.{StatusMapper, Tracer}
 
 object FieldTracer {
   val wrapper = new FieldWrapper[Tracer] {
     def wrap[R <: Tracer](
-      query: ZQuery[R, CalibanError.ExecutionError, ResponseValue],
-      info: FieldInfo
-    ): ZQuery[R, CalibanError.ExecutionError, ResponseValue] =
+                           query: ZQuery[R, CalibanError.ExecutionError, ResponseValue],
+                           info: FieldInfo
+                         ): ZQuery[R, CalibanError.ExecutionError, ResponseValue] =
       ZQuery.fromZIO(ZIO.service[Tracer]).flatMap { tracer =>
         // Use ZQuery.foldCauseQuery to handle success and failure cases
         query.foldCauseQuery(
