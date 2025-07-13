1a6c5707fb7ecb6632c6f16568fc1f4589049a7f
Ilya Brin <dontgitit@users.noreply.github.com>
2025-07-12 20:27:19 -0500

    add test that generates code that doesn't compile

--- tools/src/test/scala/caliban/tools/ClientWriterSpec.scala
+++ tools/src/test/scala/caliban/tools/ClientWriterSpec.scala
@@ -112,6 +112,16 @@ object ClientWriterSpec extends SnapshotTest {
              }
             """)
       },
+      snapshotTest("implicit encoders for String and scalar") {
+        gen(
+          """
+             type PriceRuleCustomerSelection {
+               customers(after: String, savedSearchId: ID): String
+             }
+
+             scalar ID
+            """)
+      },
       snapshotTest("schema") {
         gen("""
              schema {
