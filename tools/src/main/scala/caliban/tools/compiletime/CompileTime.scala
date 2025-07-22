1b118e7c7847f236e465eaa0442568deb0861782
Pierre Ricadat <pierre.ricadat@devsisters.com>
2025-07-22 09:30:26 +0900

    Remove usage of exitCode

--- tools/src/main/scala/caliban/tools/compiletime/CompileTime.scala
+++ tools/src/main/scala/caliban/tools/compiletime/CompileTime.scala
@@ -4,13 +4,13 @@ import caliban._
 import caliban.tools.Codegen.GenType
 import caliban.tools.compiletime.Config.ClientGenerationSettings
 import caliban.tools.{ Codegen, SchemaLoader }
-import zio.{ ExitCode, UIO, ZIO }
+import zio.{ Task, ZIO }
 
 object CompileTime {
 
   def generateClient[R](
     args: List[String]
-  )(api: GraphQL[R], settings: ClientGenerationSettings): UIO[ExitCode] =
+  )(api: GraphQL[R], settings: ClientGenerationSettings): Task[Unit] =
     args match {
       case baseDir :: Nil =>
         Codegen
@@ -26,11 +26,10 @@ object CompileTime {
             ),
             GenType.Client
           )
-          .exitCode
+          .unit
       case _              =>
         ZIO
           .fail(new RuntimeException(s"`CompileTime.generateClient` was called with invalid arguments: $args"))
-          .exitCode
     }
 
 }
