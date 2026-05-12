4e6956d2b7d2d2f0a5c6b72132a5282c72e9532e
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2026-05-12 13:31:55 +0900

    Fix escaping in Value rendering (#2961)

--- core/src/test/scala/caliban/RenderingSpec.scala
+++ core/src/test/scala/caliban/RenderingSpec.scala
@@ -232,6 +232,18 @@ object RenderingSpec extends ZIOSpecDefault {
           rendered == """schema{query:Query} "Description of custom scalar emphasizing proper captain ship names" scalar CaptainShipName@specifiedBy(url:"http://someUrl")@tag union Role@uniondirective=Captain|Engineer|Mechanic|Pilot enum Origin@enumdirective{BELT,EARTH,MARS,MOON@deprecated(reason:"Use: EARTH | MARS | BELT")} input CharacterInput@inputobjdirective{name:String!@external nicknames:[String!]!@required origin:Origin!} interface Human{name:String!@external} type Captain{shipName:CaptainShipName!} type Character implements Human@key(name:"name"){name:String!@external nicknames:[String!]!@required origin:Origin! role:Role} type Engineer{shipName:String!} type Mechanic{shipName:String!} type Narrator implements Human{name:String!} type Pilot{shipName:String!} "Queries" type Query{"Return all characters from a given origin" characters(origin:Origin):[Character!]! character(name:String!):Character@deprecated(reason:"Use `characters`") charactersIn(names:[String!]!@lowercase):[Character!]! exists(character:CharacterInput!):Boolean! human:Human!} """
         )
       },
+      test("it should escape control characters in Value toString") {
+        assertTrue(
+          Value.StringValue("hello\tworld").toString == """"hello\tworld"""",
+          Value.StringValue("a\\b").toString == """"a\\b"""",
+          Value.StringValue("line1\rline2").toString == """"line1\rline2"""",
+          Value.StringValue("back\bform\fquote\"").toString == """"back\bform\fquote\""""",
+          Value.EnumValue("hello\tworld").toString == """"hello\tworld"""",
+          Value.EnumValue("a\\b").toString == """"a\\b"""",
+          Value.EnumValue("line1\rline2").toString == """"line1\rline2"""",
+          Value.EnumValue("back\bform\fquote\"").toString == """"back\bform\fquote\"""""
+        )
+      },
       suite("round-trip")(
         test("kitchen sink")(roundTrip("document-tests/kitchen-sink.graphql")),
         test("kitchen sink with query")(roundTrip("document-tests/kitchen-sink-query.graphql")),
