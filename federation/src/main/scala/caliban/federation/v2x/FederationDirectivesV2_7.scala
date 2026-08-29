5e0c66062b7b55c6200f4dbe11309a9682ff6345
Paul Daniels <paul@leadiq.com>
2026-08-29 12:34:05 +0800

    add mima filters

--- federation/src/main/scala/caliban/federation/v2x/FederationDirectivesV2_7.scala
+++ federation/src/main/scala/caliban/federation/v2x/FederationDirectivesV2_7.scala
@@ -8,7 +8,7 @@ trait FederationDirectivesV2_7 { self: FederationDirectivesV2_6 =>
   case class GQLProgressiveOverride(from: String, label: String) extends GQLDirective(ProgressiveOverride(from, label))
 
   object ProgressiveOverride {
-    def apply(from: String, label: String): Directive =
+    final def apply(from: String, label: String): Directive =
       Directive("override", Map("from" -> StringValue(from), "label" -> StringValue(label)))
   }
 }
